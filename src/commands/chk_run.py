2960a9fa046b550ed848e0259bf99281b2e08a47
Hasan BD.DHK <mahmudul.hasan@monstar-lab.com>
2022-01-07 10:02:05 +0600

    feat: load minimum request yaml file

--- src/commands/chk_run.py
+++ src/commands/chk_run.py
@@ -1,7 +1,25 @@
 import click
+from yaml import safe_load
+from pprint import pp
 
 
 @click.command()
 @click.argument('file')
 def execute(file):
     click.echo(file)
+
+    # read yml data
+    with open(file, 'r') as yf:
+        yaml_data = safe_load(yf)
+
+    pp(yaml_data)
+    pp(yaml_data['request']['path'])
+
+    # make request
+    fetch(yaml_data['request'])
+
+    # process data
+
+
+def fetch(request: dict):
+    pass
