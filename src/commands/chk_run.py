07ddbc860347a59b5194305ee047cfe0b5940770
HSN <hasanlock@gmail.com>
2022-01-07 19:16:04 +0600

    feature: convert all dict to weak DotMap obj

--- src/commands/chk_run.py
+++ src/commands/chk_run.py
@@ -1,30 +1,31 @@
 import click
 from yaml import safe_load
 from requests import request
+from dotmap import DotMap
 
 
 @click.command()
 @click.argument('file')
 def execute(file):
     """execute command"""
-    yaml_data = read_chk(file)
-    response = make_request(yaml_data['request'])
+    doc = read_chk(file)
+    response = make_request(doc.request)
 
     # process data
     print(response.text)
 
 
-def read_chk(file_name: str) -> dict:
+def read_chk(file_name: str) -> DotMap:
     """read yml data"""
     with open(file_name, 'r') as yf:
-        return safe_load(yf)
+        return DotMap(safe_load(yf))
 
 
-def make_request(request_data: dict):
+def make_request(request_data: DotMap):
     """Make external api call"""
     response = request(
-        request_data['method'],
-        request_data['path'],
+        request_data.method,
+        request_data.path,
     )
 
     return response
