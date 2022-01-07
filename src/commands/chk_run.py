b15ffacfd333634bef60b886488e0ce63e312bac
Hasan BD.DHK <mahmudul.hasan@monstar-lab.com>
2022-01-07 11:29:36 +0600

    feature: process response after request

--- src/commands/chk_run.py
+++ src/commands/chk_run.py
@@ -1,30 +1,30 @@
 import click
 from yaml import safe_load
 from requests import request
-from pprint import pp
 
 
 @click.command()
 @click.argument('file')
 def execute(file):
-    click.echo(file)
-
-    # read yml data
-    with open(file, 'r') as yf:
-        yaml_data = safe_load(yf)
-
-    pp(yaml_data)
-
-    # make request
-    make_request(yaml_data['request'])
+    """execute command"""
+    yaml_data = read_chk(file)
+    response = make_request(yaml_data['request'])
 
     # process data
+    print(response.text)
+
+
+def read_chk(file_name: str) -> dict:
+    """read yml data"""
+    with open(file_name, 'r') as yf:
+        return safe_load(yf)
 
 
 def make_request(request_data: dict):
+    """Make external api call"""
     response = request(
         request_data['method'],
         request_data['path'],
     )
 
-    pp(response.text)
+    return response
