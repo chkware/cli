b9fddd648080c468234ba44d18517324c1eacd79
HSN <hasanlock@gmail.com>
2022-01-08 13:21:22 +0600

    feature: enum for http methods

--- support/http_requestor.py
+++ support/http_requestor.py
@@ -1,17 +1,27 @@
 from requests import request, Response
 from dotmap import DotMap
+import enum
 
 
+# module functions
 def make_request(request_data: DotMap) -> Response:
     """Make external api call"""
     request_args = get_request_args(request_data)
     return request(**request_args)
 
 
+# class
+class HttpMethod(enum.Enum):
+    """Constants of wellknown http methods"""
+    GET = 'GET',
+    POST = 'POST',
+
+
+# services
 def get_request_args(request_data: DotMap) -> dict:
     """Prepare dotmap to dict before making request"""
     _args_http = {
-        'GET': _args_http_get,
+        HttpMethod.GET: _args_http_get,
     }.get(request_data.method)
 
     if _args_http is None:
