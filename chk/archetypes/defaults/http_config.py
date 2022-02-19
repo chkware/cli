db223cb9f7398f02011e3e6ac3a34aadf9eaf8fc
HSN <hasanlock@gmail.com>
2022-02-19 10:24:35 +0600

    feature: 0.7.2 version of http file init

---
+++ chk/archetypes/defaults/http_config.py
@@ -0,0 +1,14 @@
+"""
+do
+"""
+from typing import Dict
+from chk.archetypes.defaults import ArchetypeConfig
+
+
+class V072(ArchetypeConfig):
+    """http config v0.7.2"""
+
+    @classmethod
+    def validate_schema(cls, config: Dict):
+        """create and validate schema against the dict passed"""
+        pass
