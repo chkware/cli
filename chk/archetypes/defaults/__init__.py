7e47eb7360999569266a1356172bdb55c13e8f27
HSN <hasanlock@gmail.com>
2022-02-19 10:24:11 +0600

    feature: abstract super for archetype config

---
+++ chk/archetypes/defaults/__init__.py
@@ -0,0 +1,24 @@
+"""
+Default archetypes
+"""
+from abc import ABC, abstractmethod
+from typing import Dict
+from chk.constants.archetype import ArchetypeConfigModules
+
+
+class ArchetypeConfig(ABC):
+    """Base class to all archetype"""
+
+    @classmethod
+    def validate_version(cls, config: Dict) -> Dict:
+        """check if this version is supported"""
+        if v := config.get('version') is not None:
+            archetype_class = ArchetypeConfigModules.data.get(v)
+            if archetype_class is None: raise SystemExit('Version not supported.')
+            return config
+
+    @classmethod
+    @abstractmethod
+    def validate_schema(cls, config: Dict):
+        """abstract method to be implemented by child"""
+        pass
