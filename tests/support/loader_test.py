a041eda0dd7c96e34de274b4bae39b9ae2ffc937
HSN <hasanlock@gmail.com>
2022-02-20 21:35:09 +0600

    refactor: move __init__.py to top level
    - update usage

--- tests/support/loader_test.py
+++ tests/support/loader_test.py
@@ -1,7 +1,7 @@
 """test loader"""
 import pytest
+import tests
 from chk.support.loader import ChkFileLoader
-from . import RES_DIR
 
 
 class TestChkFileLoader:
@@ -9,26 +9,26 @@ class TestChkFileLoader:
 
     def test_to_dict_valid_file(self):
         """test to_dict with valid file"""
-        filename = RES_DIR + "UserOk.chk"
+        filename = tests.RES_DIR + "UserOk.chk"
         assert type(ChkFileLoader.to_dict(filename)) == dict
 
     def test_to_dict_invalid_file(self):
         """test with invalid file"""
-        filename = RES_DIR + "UserNotOk.chk"
+        filename = tests.RES_DIR + "UserNotOk.chk"
         with pytest.raises(SystemExit):
             assert type(ChkFileLoader.to_dict(filename)) == dict
 
     def test_is_file_ok_valid_file(self):
         """test is_file_ok with valid existing file"""
-        filename = RES_DIR + "UserOk.chk"
+        filename = tests.RES_DIR + "UserOk.chk"
         assert ChkFileLoader.is_file_ok(filename) is True
 
     def test_is_file_ok_invalid_file(self):
         """test to_dict"""
-        filename = RES_DIR + "UserOk.yaml"
+        filename = tests.RES_DIR + "UserOk.yaml"
         assert ChkFileLoader.is_file_ok(filename) is False
 
     def test_is_file_ok_inexistent_file(self):
         """test to_dict"""
-        filename = RES_DIR + "UserOk.yml"
+        filename = tests.RES_DIR + "UserOk.yml"
         assert ChkFileLoader.is_file_ok(filename) is False
