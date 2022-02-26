88232add009d7a5018ed76fd8ff9e60d731dea91
HSN <hasanlock@gmail.com>
2022-02-26 13:18:49 +0600

    refactor: remove unused message

--- chk/constants/messages/error.py
+++ chk/constants/messages/error.py
@@ -4,8 +4,6 @@ english messages
 messages = dict(
     fatal=dict(
         V0001='`version` string not found.',
-        V0002='Version not supported.',
         V0003='`{file_name}` is not a valid YAML.',
-        V0004='`version` string cannot be empty.',
     )
 )
