de14575cd447e8315c3ca3cdf6236455466b24df
HSN <hasanlock@gmail.com>
2022-02-26 13:16:18 +0600

    refactor: die with plain system error
    - think it's better for early stage

--- chk/archetypes/defaults/doc_config.py
+++ chk/archetypes/defaults/doc_config.py
@@ -1,11 +1,9 @@
 """
 Versioned schema repository for http specifications
 """
-from types import NoneType
 from typing import Dict
 from chk.archetypes.defaults import ArchetypeConfig
 from chk.constants.archetype.validation import version_schema
-import cerberus.errors
 from cerberus.validator import DocumentError
 from chk.globals import current_app
 
@@ -24,19 +22,7 @@ class DocV072(ArchetypeConfig):
 
         try:
             if self.validator.validate(config, self.get_schema()) is not True:
-                if cerberus.errors.EMPTY_NOT_ALLOWED \
-                        in self.validator.document_error_tree['version']:
-                    raise SystemExit(app.config.error.fatal.V0004)
-
-                if cerberus.errors.UNALLOWED_VALUE \
-                        in self.validator.document_error_tree['version']:
-                    raise SystemExit(app.config.error.fatal.V0002)
-
-                if cerberus.errors.REQUIRED_FIELD \
-                        in self.validator.document_error_tree['version']:
-                    raise SystemExit(app.config.error.fatal.V0002)
-
-                return False
+                raise SystemExit(str(self.validator.errors))
         except DocumentError as doc_err:
             raise SystemExit(f'{app.config.error.fatal.V0001}: {doc_err}') from doc_err
         else:
