5b32171aeab942fc6c25231e5a35caea5cd9fb1b
HSN <hasanlock@gmail.com>
2022-03-13 17:29:08 +0600

    refactor: simplify global app context impl
    - move exception to separate ns

---
+++ chk/infrastructure/exception.py
@@ -0,0 +1,10 @@
+"""
+exception messages
+"""
+messages = dict(
+    fatal=dict(
+        V0001='`version` string not found.',
+        V0002='Invalid file format.',
+        V0003='`{file_name}` is not a valid YAML.',
+    )
+)
