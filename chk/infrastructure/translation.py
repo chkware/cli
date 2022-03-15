3469656764218e3d9cabc74896e722ceadfce896
HSN <hasanlock@gmail.com>
2022-03-15 20:42:00 +0600

    feature: translation helper test

--- chk/infrastructure/translation.py
+++ chk/infrastructure/translation.py
@@ -4,6 +4,10 @@ from typing import Dict
 
 def l10n(message: str, data: Dict | None = None) -> str:
     """get localized message from locale file"""
+
+    if message is None: raise ValueError
+    if not message: raise ValueError
+
     message = str(message)
     if data is None: data = {}
 
---
+++ tests/infrastructure/translation_test.py
@@ -0,0 +1,21 @@
+"""
+test global chk functions
+"""
+import pytest
+
+from chk.console.app_container import app
+from chk.infrastructure.translation import l10n
+
+
+class TestTranslation:
+    """Test chk functions"""
+    def test_l10n_should_pass(self):
+        retstr = l10n(app.messages.exception.key, {'name': 'Hasan'})
+
+        assert retstr == 'Some name: Hasan'
+
+    def test_l10n_should_fail_on_empty_message(self):
+        with pytest.raises(ValueError):
+            l10n(None, {'name': 'Hasan'})
+
+
