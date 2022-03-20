476c3bbe3ae62fae1763e9a0dcb1b95068648ca3
HSN <hasanlock@gmail.com>
2022-03-21 00:00:41 +0600

    feature: introduce document
    - get document version utils
    - move BaseDocElements to version module
    - BaseDocElements:find_by_version

--- chk/modules/version/constants.py
+++ chk/modules/version/constants.py
@@ -1,4 +1,10 @@
+from chk.console.app_container import app
 from typing import Dict
+
+
+class BaseDocElements:
+    """represent the base of all kind of documents"""
+    VERSION = 'version'
 
 
 class VersionStrToSpecConfigMapping:
@@ -7,3 +13,14 @@ class VersionStrToSpecConfigMapping:
     data: Dict = {
         "default:http:0.7.2": "chk.modules.http.entities.HttpConfigV072",
     }
+
+    @classmethod
+    def find_by_version(cls, key: str) -> str:
+        """find config by version str"""
+        if not isinstance(key, str): raise SystemExit(app.messages.exception.fatal.V0004)
+
+        class_map = cls.data.get(key)
+        if not isinstance(class_map, str): raise SystemExit(app.messages.exception.fatal.V0004)
+        elif len(class_map) == 0: raise SystemExit(app.messages.exception.fatal.V0004)
+
+        return class_map
--- chk/modules/version/entities.py
+++ chk/modules/version/entities.py
@@ -1,11 +1,13 @@
 """
 Shared entities
 """
+import pydoc
 from abc import ABC, abstractmethod
 from cerberus import Validator
 from cerberus.validator import DocumentError
 from chk.console.app_container import app
 from chk.modules.version.validation_rules import version_schema
+from chk.modules.version.constants import BaseDocElements, VersionStrToSpecConfigMapping as Mapping
 from typing import Dict
 
 
@@ -13,6 +15,7 @@ class AbstractSpecConfig(ABC):
     """Base class to all archetype"""
 
     def __init__(self):
+        self.document: Dict = {}
         self.validator = Validator()
 
     @abstractmethod
@@ -20,8 +23,18 @@ class AbstractSpecConfig(ABC):
         """abstract method to be implemented by child"""
 
     @abstractmethod
-    def validate_config(self, config: Dict) -> bool:
+    def validate_config(self) -> bool:
         """Error handling at global level for schemas"""
+
+    @classmethod
+    def from_version(cls, version: str = ''):
+        """Creates and returns AbstractSpecConfig from version stirng"""
+        class_map = Mapping.find_by_version(version)
+        http_config_class = pydoc.locate(class_map)
+
+        if not callable(http_config_class): raise SystemExit('Not callable.')
+
+        return http_config_class()
 
 
 class VersionConfigV072(AbstractSpecConfig):
@@ -34,14 +47,21 @@ class VersionConfigV072(AbstractSpecConfig):
         """create and validate schema against the dict passed"""
         return version_schema
 
-    def validate_config(self, config: Dict) -> bool:
+    def validate_config(self) -> bool:
         """Validate the schema against config"""
         self.validator.allow_unknown = True
 
         try:
-            if self.validator.validate(config, self.get_schema()) is not True:
+            if not self.validator.validate(self.document, self.get_schema()):
                 raise SystemExit(str(self.validator.errors))
         except DocumentError as doc_err:
             raise SystemExit(f'{app.messages.exception.fatal.V0001}: {doc_err}') from doc_err
         else:
             return True  # or is a success
+
+
+def get_document_version(document: Dict) -> str:
+    """check and get version string"""
+    if not isinstance(document, dict): raise SystemExit(app.messages.exception.fatal.V0005)
+
+    return str(document.get(BaseDocElements.VERSION))
