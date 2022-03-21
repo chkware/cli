1fba6e7b4940b0144d22b35569a728525908fd86
HSN <hasanlock@gmail.com>
2022-03-21 23:48:41 +0600

    refactor: handle file type check in is_file_ok

--- chk/infrastructure/file_loader.py
+++ chk/infrastructure/file_loader.py
@@ -1,8 +1,7 @@
-from chk.console.app_container import app
-from chk.infrastructure.translation import l10n
 from pathlib import Path
 from typing import Dict
 from yaml import safe_load
+from chk.infrastructure import exception
 
 
 class ChkFileLoader:
@@ -15,9 +14,13 @@ class ChkFileLoader:
             try:
                 return safe_load(yaml_file)
             except:
-                raise SystemExit(l10n(app.messages.exception.fatal.V0003, {'file_name': file_name}))
+                raise SystemExit(exception.err_message('fatal.V0003', {'file_name': file_name}))
 
     @staticmethod
     def is_file_ok(file_name: str) -> bool:
         """Check if chk file exists, extension is okay"""
-        return Path(file_name).is_file() and Path(file_name).suffix == '.chk'
+        if Path(file_name).is_file() and Path(file_name).suffix == '.chk':
+            return True
+
+        raise SystemExit(exception.err_message('fatal.V0002'))
+
