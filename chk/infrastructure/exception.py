3e4bdc27104b52bba6a0174cb271a46336f03dcc
HSN <hasanlock@gmail.com>
2022-03-22 00:11:27 +0600

    refactor: update error messages

--- chk/infrastructure/exception.py
+++ chk/infrastructure/exception.py
@@ -9,9 +9,10 @@ messages = dict(
         V0001='`version` string not found.',
         V0002='Invalid file format.',
         V0003='`{file_name}` is not a valid YAML.',
-        V0004='Unsupported document version',
-        V0005='Unsupported document',
+        V0004='Unsupported document version.',
+        V0005='Unsupported document.',
         V0006='Validation error.',
+        V0007='Not a Callable.',
     )
 )
 
