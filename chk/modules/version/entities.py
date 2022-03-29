5a54051de3aefde7b864392b4f7e4e815b13159f
HSN <hasanlock@gmail.com>
2022-03-29 23:28:38 +0600

    mixin for version and request

--- chk/modules/version/entities.py
+++ chk/modules/version/entities.py
@@ -68,11 +68,33 @@ def get_document_version(document: Dict) -> str:
     return str(document.get(BaseDocElements.VERSION))
 
 
+# --------------
+
 class VersionMixin_V072:
     """ Mixin for version spec. for v0.7.2"""
 
-    def _get_validation_schema(self):
+    def rules(self) -> Dict:
         """Get validation schema"""
         return version_schema
     
-    
+    def validate(self) -> bool:
+        """Validate the schema against config"""
+        if not hasattr(self, 'validator') or not hasattr(self, 'document'):
+            raise SystemExit(err_message('fatal.V0005'))
+        
+        self.validator.allow_unknown = True
+
+        try:
+            if not self.validator.validate(self.document, self.rules()):
+                raise SystemExit(err_message('fatal.V0006', extra=self.validator.errors))
+        except DocumentError as doc_err:
+            raise SystemExit(err_message('fatal.V0001', extra=doc_err)) from doc_err
+        else:
+            return True  # or is a success
+
+    def get(self) -> str:
+        """Get version string"""
+        try:
+            return str(self.document.get(BaseDocElements.VERSION))
+        except:
+            raise SystemExit(err_message('fatal.V0005'))
