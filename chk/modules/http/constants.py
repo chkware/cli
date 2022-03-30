548acb1662f499e429fa2ae6f093b36b7b7bac8e
HSN <hasanlock@gmail.com>
2022-03-30 10:38:02 +0600

    refactor: remove old version classes

--- chk/modules/http/constants.py
+++ chk/modules/http/constants.py
@@ -1,11 +1,9 @@
 """
 Constants used in http modules
 """
-from enum import Enum
-from chk.modules.version import constants as ver_const
 
 
-class HttpMethod(Enum):
+class HttpMethod:
     """Constants of wellknown http methods"""
     GET = 'GET'
     POST = 'POST'
@@ -14,30 +12,6 @@ class HttpMethod(Enum):
     DELETE = 'DELETE'
     HEAD = 'HEAD'
     OPTIONS = 'OPTIONS'
-
-
-class HttpDocElements(ver_const.BaseDocElements):
-    """represent http documents"""
-    # common request
-    URL = 'url'
-    METHOD = 'method'
-    HEADERS = 'headers'
-    PARAMS = 'url_params'
-
-    # Basic
-    AUTH_BA = 'auth[basic]'
-    AUTH_BA_USR = 'username'
-    AUTH_BA_PAS = 'password'
-
-    # Bearer
-    AUTH_BE = 'auth[bearer]'
-    AUTH_BE_TOK = 'token'
-
-    # Body
-    BODY_FRM = 'body[form]'
-    BODY_FRM_DAT = 'body[form-data]'
-    BODY_JSN = 'body[json]'
-    BODY_XML = 'body[xml]'
 
 
 class RequestConfigElements_V072:
--- chk/modules/version/entities.py
+++ chk/modules/version/entities.py
@@ -2,29 +2,21 @@
 Shared entities
 """
 import pydoc
-from abc import ABC, abstractmethod
+from abc import ABC
 from cerberus import Validator
 from cerberus.validator import DocumentError
 from chk.infrastructure.exception import err_message
 from chk.modules.version.validation_rules import version_schema
-from chk.modules.version.constants import BaseConfigElements, BaseDocElements, VersionStrToSpecConfigMapping as Mapping
+from chk.modules.version.constants import BaseConfigElements, VersionStrToSpecConfigMapping as Mapping
 from typing import Dict
 
 
-class AbstractSpecConfig(ABC):
+class BaseSpecConfig(ABC):
     """Base class to all archetype"""
 
     def __init__(self):
         self.document: Dict = {}
         self.validator = Validator()
-
-    @abstractmethod
-    def get_schema(self) -> Dict:
-        """abstract method to be implemented by child"""
-
-    @abstractmethod
-    def validate_config(self) -> bool:
-        """Error handling at global level for schemas"""
 
     @classmethod
     def from_version(cls, version: str = ''):
@@ -38,37 +30,6 @@ class AbstractSpecConfig(ABC):
         return http_config_class()
 
 
-class VersionConfigV072(AbstractSpecConfig):
-    """
-    Versioned schema repository for http specifications
-    version: v0.7.2
-    """
-
-    def get_schema(self) -> Dict:
-        """create and validate schema against the dict passed"""
-        return version_schema
-
-    def validate_config(self) -> bool:
-        """Validate the schema against config"""
-        self.validator.allow_unknown = True
-
-        try:
-            if not self.validator.validate(self.document, self.get_schema()):
-                raise SystemExit(err_message('fatal.V0006', extra=self.validator.errors))
-        except DocumentError as doc_err:
-            raise SystemExit(err_message('fatal.V0001', extra=doc_err)) from doc_err
-        else:
-            return True  # or is a success
-
-
-def get_document_version(document: Dict) -> str:
-    """check and get version string"""
-    if not isinstance(document, dict): raise SystemExit(err_message('fatal.V0005'))
-
-    return str(document.get(BaseDocElements.VERSION))
-
-
-# --------------
 class VersionMixin_V072:
     """ Mixin for version spec. for v0.7.2"""
 
