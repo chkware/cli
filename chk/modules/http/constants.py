9d7894d1ab49fd7fab7cceb484a92117b02ad5e1
HSN <hasanlock@gmail.com>
2022-03-30 10:09:48 +0600

    feature: add as_dict to request and constant

--- chk/modules/http/constants.py
+++ chk/modules/http/constants.py
@@ -38,3 +38,29 @@ class HttpDocElements(ver_const.BaseDocElements):
     BODY_FRM_DAT = 'body[form-data]'
     BODY_JSN = 'body[json]'
     BODY_XML = 'body[xml]'
+
+
+class RequestConfigElements_V072:
+    """represent request config section"""
+    ROOT = 'request'
+
+    # common request
+    URL = 'url'
+    METHOD = 'method'
+    HEADERS = 'headers'
+    PARAMS = 'url_params'
+
+    # Basic
+    AUTH_BA = 'auth[basic]'
+    AUTH_BA_USR = 'username'
+    AUTH_BA_PAS = 'password'
+
+    # Bearer
+    AUTH_BE = 'auth[bearer]'
+    AUTH_BE_TOK = 'token'
+
+    # Body
+    BODY_FRM = 'body[form]'
+    BODY_FRM_DAT = 'body[form-data]'
+    BODY_JSN = 'body[json]'
+    BODY_XML = 'body[xml]'
--- chk/modules/http/entities.py
+++ chk/modules/http/entities.py
@@ -6,10 +6,12 @@ from chk.modules.version.entities import AbstractSpecConfig, VersionConfigV072
 from chk.modules.http.validation_rules import request_schema
 from cerberus.validator import DocumentError
 from chk.infrastructure.exception import err_message
+from chk.modules.http.constants import RequestConfigElements_V072
 
 
 class HttpConfigV072(AbstractSpecConfig):
     """http config v0.7.2"""
+
     def __init__(self):
         super().__init__()
         self.version_config = VersionConfigV072()
@@ -31,23 +33,32 @@ class HttpConfigV072(AbstractSpecConfig):
         else:
             return True  # or is a success
 
+
 # --------------
 
 class RequestMixin_V072:
-    
+    """ Mixin for request spec. for v0.7.2"""
     def rules(self) -> Dict:
         """ get schema"""
         return request_schema
-    
-    def validate(self) -> Dict:
+
+    def validated(self) -> dict[str, dict]:
         """Validate the schema against config"""
-        if not hasattr(self, 'validator') or not hasattr(self, 'document'):
-            raise SystemExit(err_message('fatal.V0005'))
-        
         try:
-            if not self.validator.validate(self.document, self.rules()):  # validate request
+            request_doc = self.as_dict()
+            if not self.validator.validate(request_doc, self.rules()):  # validate request
                 raise SystemExit(err_message('fatal.V0006', extra=self.validator.errors))
         except DocumentError as doc_err:
             raise SystemExit(err_message('fatal.V0001', extra=doc_err)) from doc_err
         else:
-            return True  # or is a success
+            return request_doc  # or is a success
+
+    def as_dict(self) -> dict[str, dict]:
+        """Get version string"""
+        if not hasattr(self, 'validator') or not hasattr(self, 'document'):
+            raise SystemExit(err_message('fatal.V0005'))
+
+        try:
+            return {RequestConfigElements_V072.ROOT: dict(self.document[RequestConfigElements_V072.ROOT])}
+        except:
+            raise SystemExit(err_message('fatal.V0005'))
