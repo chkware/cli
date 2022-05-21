6fb3051849a40b341adebedc7f7e4b63b844a192
HSN <hasanlock@gmail.com>
2022-05-21 12:52:17 +0600

    feat: ExecutionContext named tuple

--- chk/modules/execute/support.py
+++ chk/modules/execute/support.py
@@ -1,10 +1,15 @@
 """
 execute module
 """
+from collections import namedtuple
 
 
 def _parse_args() -> list[str]:
     import sys
     if len(sys.argv) > 3: return sys.argv[3:]
     return []
+
+
+# File context that holds file information
+ExecutionContext = namedtuple('ExecutionContext', ['filepath', 'with_data', 'result'])
 
