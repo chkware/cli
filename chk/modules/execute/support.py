9f8e635f8f4bdb663fda5c781eeb6e79cac2ca93
HSN <hasanlock@gmail.com>
2022-05-21 14:28:04 +0600

    feature: improved argument parsing

--- chk/modules/execute/support.py
+++ chk/modules/execute/support.py
@@ -4,10 +4,18 @@ execute module
 from collections import namedtuple
 
 
-def _parse_args() -> list[str]:
+def _parse_args() -> dict[str, str]:
+    """
+    parse and return args to dict
+    :return: dict
+    """
     import sys
-    if len(sys.argv) > 3: return sys.argv[3:]
-    return []
+    if len(sys.argv) > 3:
+        c_args = sys.argv[3:]
+        c_args = list(filter(lambda x: True if '=' in x else False, c_args))
+        return {arg_i[0]:arg_i[1] for arg_i in list(map(lambda x: x.split('='), c_args)) }
+
+    return {}
 
 
 # File context that holds file information
