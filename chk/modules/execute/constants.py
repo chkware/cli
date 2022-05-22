784425c88c02fda267915c26d48bfb72248e5be8
HSN <hasanlock@gmail.com>
2022-05-22 14:06:31 +0600

    feat: execute context builder from dict

---
+++ chk/modules/execute/constants.py
@@ -0,0 +1,11 @@
+"""
+config and constants for `execute:`
+"""
+
+
+class ExecuteConfigNode:
+    """represent request config section"""
+    ROOT = 'execute'
+    FILE = 'file'
+    WITH = 'with'
+    RESULT = 'result'
--- chk/modules/execute/support.py
+++ chk/modules/execute/support.py
@@ -1,7 +1,12 @@
 """
 execute module
 """
+from chk.modules.execute.constants import ExecuteConfigNode
 from collections import namedtuple
+
+
+# File context that holds file information
+ExecutionContext = namedtuple('ExecutionContext', ['file', 'variables', 'result'])
 
 
 def _parse_args() -> dict[str, str]:
@@ -13,11 +18,22 @@ def _parse_args() -> dict[str, str]:
     if len(sys.argv) > 3:
         c_args = sys.argv[3:]
         c_args = list(filter(lambda x: True if '=' in x else False, c_args))
-        return {arg_i[0]:arg_i[1] for arg_i in list(map(lambda x: x.split('='), c_args)) }
+        return {arg_i[0]:arg_i[1] for arg_i in list(map(lambda x: x.split('='), c_args))}
 
     return {}
 
 
-# File context that holds file information
-ExecutionContext = namedtuple('ExecutionContext', ['filepath', 'with_data', 'result'])
+class ExecutionContextBuilder:
+    """
+    Build `ExecutionContext` from different execution context
+    """
+    @staticmethod
+    def from_dict(document: dict[str, object]) -> ExecutionContext:
+        file = document.get(ExecuteConfigNode.FILE)
+        if not file: raise SystemExit(f'`{ExecuteConfigNode.FILE}` is required')
 
+        return ExecutionContext(
+            file,
+            document.get(ExecuteConfigNode.WITH),
+            document.get(ExecuteConfigNode.RESULT)
+        )
