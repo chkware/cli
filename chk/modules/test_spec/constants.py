2f66dbb7f72cccaa6876e15273d862501230f27d
HSN <hasanlock@gmail.com>
2022-06-09 21:31:13 +0600

    feature: validation rules and constants for test_spec

---
+++ chk/modules/test_spec/constants.py
@@ -0,0 +1,12 @@
+"""
+Constants used in test_spec modules
+"""
+
+
+class TestSpecConfigNode:
+    """represent request config section"""
+    ROOT = 'spec'
+
+    # common request
+    EXECUTE = 'execute'
+    ASSERTS = 'asserts'
