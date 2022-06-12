9791f75a3914026a9c97aca774fafbb76e9b0969
HSN <hasanlock@gmail.com>
2022-06-12 16:22:23 +0600

    feature: TestHttpSpec_V072 pass

--- tests/modules/http/http_entities_test.py
+++ tests/modules/http/http_entities_test.py
@@ -1,8 +1,15 @@
-import pytest
 import tests
+
 from chk.modules.http.entities import HttpSpec_V072
-from chk.infrastructure.file_loader import ChkFileLoader
+from chk.infrastructure.file_loader import ChkFileLoader, FileContext
+from chk.infrastructure.work import handle_worker
 
 
-class TestValidationForRequest_HttpSpec_V072:
-    pass
+class TestHttpSpec_V072:
+    def test_get_plain_pass(self):
+        doc = ChkFileLoader.to_dict(tests.RES_DIR + 'pass_cases/GET-Plain.chk')
+        file_ctx = FileContext(None, None, None, doc)
+
+        http = HttpSpec_V072(file_ctx)
+        response = handle_worker(http)
+        assert type(response) == dict
