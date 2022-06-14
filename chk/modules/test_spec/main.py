b1b8f5d87b699e4083ca4d29ace363f17df8e5da
HSN <hasanlock@gmail.com>
2022-06-14 14:03:06 +0600

    refactor: update main driver for test_spec

--- chk/modules/test_spec/main.py
+++ chk/modules/test_spec/main.py
@@ -2,23 +2,19 @@
 test_spec module's driver
 """
 from chk.infrastructure.file_loader import ChkFileLoader, FileContext
-from chk.modules.http.presentation import ResponseToStringFormatter
+from chk.modules.http.presentation import make_displayable
 from chk.modules.version.support import SpecificationLoader
 from chk.infrastructure.work import handle_worker
 
 
 def execute(file: str):
-    """
-    execute command
-    """
+    """ execute command """
     ChkFileLoader.is_file_ok(file)
     document = ChkFileLoader.to_dict(file)
     fpath_mangled, fpath_hash = ChkFileLoader.get_mangled_name(file)
 
     file_ctx = FileContext(file, fpath_mangled, fpath_hash, document)
-    test_spec = SpecificationLoader.to_spec_config(file_ctx)
+    http_spec = SpecificationLoader.to_spec_config(file_ctx)
 
-    handle_worker(test_spec)
-    fmt_str = ResponseToStringFormatter(test_spec.response)
-
-    print(fmt_str.get())  # print data
+    response = handle_worker(http_spec)
+    print(make_displayable(response))  # print data
