d03aac5948ab4f96e631166b297b4abb340942f1
HSN <hasanlock@gmail.com>
2022-06-18 14:25:24 +0600

    feature: validation rule for execute schema

--- chk/modules/test_spec/validation_rules.py
+++ chk/modules/test_spec/validation_rules.py
@@ -1,9 +1,38 @@
 """
 Validation rules and supporting libs for http module
 """
-from chk.modules.test_spec.constants import TestSpecConfigNode
+from chk.modules.test_spec.constants import TestSpecConfigNode, ExecuteConfigNode
 
-test_spec_schema = {  # cerberus validation rules
+execute_schema = {
+    ExecuteConfigNode.ROOT: {
+        'required': True,
+        'empty': False,
+        'nullable': False,
+        'type': 'dict',
+        'schema': {
+            ExecuteConfigNode.FILE: {
+                'required': False,
+                'empty': False,
+                'nullable': True,
+                'type': 'string',
+            },
+            ExecuteConfigNode.WITH: {
+                'required': False,
+                'empty': False,
+                'nullable': True,
+                'type': 'dict',
+            },
+            ExecuteConfigNode.RESULT: {
+                'required': True,
+                'empty': False,
+                'nullable': False,
+                'type': 'string',
+            }
+        }
+    }
+}
+
+test_spec_schema = {
     TestSpecConfigNode.ROOT: {
         'required': True,
         'type': 'dict',
@@ -12,6 +41,7 @@ test_spec_schema = {  # cerberus validation rules
                 'required': False,
                 'empty': False,
                 'type': 'dict',
+                'schema': execute_schema,
             },
             TestSpecConfigNode.ASSERTS: {
                 'required': True,
