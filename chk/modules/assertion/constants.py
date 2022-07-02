652bba45398eb34aa273b4c724ae0281041959d5
HSN <hasanlock@gmail.com>
2022-07-02 19:59:04 +0600

    feature: new assertion module init

---
+++ chk/modules/assertion/constants.py
@@ -0,0 +1,8 @@
+"""
+Constants used in assertion module
+"""
+
+
+class AssertConfigNode:
+    """represent request config section"""
+    ROOT = 'asserts'
---
+++ chk/modules/assertion/support.py
@@ -0,0 +1,14 @@
+"""
+Assertion related services
+"""
+from chk.infrastructure.exception import err_message
+from chk.modules.assertion.constants import AssertConfigNode
+
+
+class AssertionMixin:
+    """
+    Assertion related helper
+    """
+
+    def assertion_handle(self):
+        pass
