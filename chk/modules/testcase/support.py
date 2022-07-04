a2551850921be1472be536d6b347cf87cc2a9e42
HSN <hasanlock@gmail.com>
2022-07-04 10:37:37 +0600

    feature: execute mixin
    - feature: execute mixin
    - feature: Testcase mixin inherits execute mixin
    - refactor: find long keys in dot notation

--- chk/modules/testcase/support.py
+++ chk/modules/testcase/support.py
@@ -3,14 +3,35 @@ testcase related support services
 """
 from cerberus import validator
 from chk.infrastructure.exception import err_message, messages
+
 from chk.modules.testcase.constants import TestSpecConfigNode, ExecuteConfigNode
 from chk.modules.http.constants import RequestConfigNode
 from chk.modules.testcase.validation_rules import testcase_schema
 
+from chk.console.helper import dict_get
 
-class TestSpecMixin:
+
+class ExecuteMixin:
     """
-    Mixin for version spec. for v0.7.2
+    Mixin for Execute sub-spec
+    """
+    def execute_as_dict(self) -> dict[str, str]:
+        """
+        Get execute as dict
+        """
+        try:
+            if spec := self.document.get(TestSpecConfigNode.ROOT):
+                if execute := spec.get(ExecuteConfigNode.ROOT):
+                    return {ExecuteConfigNode.ROOT: execute}
+                else:
+                    raise ValueError({'spec': [{'execute': ['required field']}]})
+        except Exception as ex:
+            raise SystemExit(err_message('fatal.V0005', extra=ex))
+
+
+class TestSpecMixin(ExecuteMixin):
+    """
+    Mixin for Testcase spec
     """
 
     def __init_testcase_mixin(self):
@@ -53,10 +74,8 @@ class TestSpecMixin:
         If request in file validate it or set False to internal in_file
         :return: None
         """
-        out_file_request = self.document\
-                               .get(TestSpecConfigNode.ROOT)\
-                               .get(ExecuteConfigNode.ROOT)\
-                               .get(ExecuteConfigNode.FILE) is not None
+        keys = [TestSpecConfigNode.ROOT, ExecuteConfigNode.ROOT, ExecuteConfigNode.FILE]
+        out_file_request = dict_get(self.document, '.'.join(keys)) is not None
         in_file_request = self.document.get(RequestConfigNode.ROOT) is not None
 
         if in_file_request is out_file_request:
@@ -77,10 +96,8 @@ class TestSpecMixin:
         """
         in_file_request = self.document.get(RequestConfigNode.ROOT) is not None
 
-        out_file_with = self.document\
-                            .get(TestSpecConfigNode.ROOT)\
-                            .get(ExecuteConfigNode.ROOT)\
-                            .get(ExecuteConfigNode.WITH) is not None
+        keys = [TestSpecConfigNode.ROOT, ExecuteConfigNode.ROOT, ExecuteConfigNode.WITH]
+        out_file_with = dict_get(self.document, '.'.join(keys)) is not None
 
         if in_file_request and out_file_with:
             raise SystemExit(err_message('fatal.V0021', extra={'spec': {'execute': {'with': 'Not allowed'}}}))
