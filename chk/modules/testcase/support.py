7f6ac3e00217a3c3f2c07bf215af8d0538d9555f
HSN <hasanlock@gmail.com>
2022-07-09 16:10:21 +0600

    refactor: fix code format

--- chk/modules/testcase/support.py
+++ chk/modules/testcase/support.py
@@ -17,6 +17,7 @@ class ExecuteMixin:
     """
     Mixin for Execute sub-spec
     """
+
     def execute_as_dict(self) -> dict[str, str]:
         """
         Get execute as dict
@@ -26,37 +27,40 @@ class ExecuteMixin:
                 if execute := spec.get(ExecuteConfigNode.ROOT):
                     return {ExecuteConfigNode.ROOT: execute}
                 else:
-                    raise ValueError({'spec': [{'execute': ['required field']}]})
+                    raise ValueError({"spec": [{"execute": ["required field"]}]})
         except Exception as ex:
-            raise SystemExit(err_message('fatal.V0005', extra=ex))
+            raise SystemExit(err_message("fatal.V0005", extra=ex))
 
 
 class AssertionMixin:
     """
     Mixin for Execute sub-spec
     """
-    def assertions_as_dict(self) -> dict[str, str]:
+
+    def assertions_as_dict(self) -> dict[str, object]:
         """
         Get execute as dict
         """
         try:
             doc_key = [TestSpecConfigNode.ROOT, TestSpecConfigNode.ASSERTS]
-            if asserts := dict_get(self.document, '.'.join(doc_key)):
+            if asserts := dict_get(self.document, ".".join(doc_key)):
                 if type(asserts) != list:
-                    raise TypeError({'spec': [{'asserts': ['expected `list`']}]})
+                    raise TypeError({"spec": [{"asserts": ["expected `list`"]}]})
 
                 return {TestSpecConfigNode.ASSERTS: asserts}
             else:
-                raise ValueError({'spec': [{'asserts': ['required field']}]})
+                raise ValueError({"spec": [{"asserts": ["required field"]}]})
         except Exception as ex:
-            raise SystemExit(err_message('fatal.V0005', extra=ex))
+            raise SystemExit(err_message("fatal.V0005", extra=ex))
 
     def assertion_process(self) -> dict:
         """
         Run assertion of testcase spec
         :return:
         """
-        pass
+        assertions = dict_get(self.assertions_as_dict(), TestSpecConfigNode.ASSERTS)
+
+        print(assertions)
 
 
 class TestSpecMixin(ExecuteMixin, AssertionMixin):
@@ -66,9 +70,9 @@ class TestSpecMixin(ExecuteMixin, AssertionMixin):
 
     def __init_testcase_mixin(self):
         self.in_file = True
-        
+
     def is_request_infile(self) -> bool:
-        return self.in_file;
+        return self.in_file
 
     def testcase_validated(self) -> dict[str, str]:
         """
@@ -77,13 +81,15 @@ class TestSpecMixin(ExecuteMixin, AssertionMixin):
         try:
             testcase_doc = self.testcase_as_dict()
             if not self.validator.validate(testcase_doc, testcase_schema):
-                raise SystemExit(err_message('fatal.V0006', extra=self.validator.errors))
+                raise SystemExit(
+                    err_message("fatal.V0006", extra=self.validator.errors)
+                )
 
             # validate request if it exists in-file
             self.validate_request_block()
             self.validate_with_block()
         except validator.DocumentError as doc_err:
-            raise SystemExit(err_message('fatal.V0001', extra=doc_err)) from doc_err
+            raise SystemExit(err_message("fatal.V0001", extra=doc_err)) from doc_err
 
         return testcase_doc  # or is a success
 
@@ -91,13 +97,17 @@ class TestSpecMixin(ExecuteMixin, AssertionMixin):
         """
         Get version string
         """
-        if not hasattr(self, 'validator') or not hasattr(self, 'document'):
-            raise SystemExit(err_message('fatal.V0005'))
+        if not hasattr(self, "validator") or not hasattr(self, "document"):
+            raise SystemExit(err_message("fatal.V0005"))
 
         try:
-            return {key: self.document[key] for key in (TestSpecConfigNode.ROOT, ) if key in self.document}
+            return {
+                key: self.document[key]
+                for key in (TestSpecConfigNode.ROOT,)
+                if key in self.document
+            }
         except Exception as ex:
-            raise SystemExit(err_message('fatal.V0005', extra=ex))
+            raise SystemExit(err_message("fatal.V0005", extra=ex))
 
     def validate_request_block(self) -> None:
         """
@@ -105,11 +115,13 @@ class TestSpecMixin(ExecuteMixin, AssertionMixin):
         :return: None
         """
         keys = [TestSpecConfigNode.ROOT, ExecuteConfigNode.ROOT, ExecuteConfigNode.FILE]
-        out_file_request = dict_get(self.document, '.'.join(keys)) is not None
+        out_file_request = dict_get(self.document, ".".join(keys)) is not None
         in_file_request = self.document.get(RequestConfigNode.ROOT) is not None
 
         if in_file_request is out_file_request:
-            raise SystemExit(err_message('fatal.V0020', extra={'spec': {'execute': {'file': '...'}}}))
+            raise SystemExit(
+                err_message("fatal.V0020", extra={"spec": {"execute": {"file": "..."}}})
+            )
 
         self.__init_testcase_mixin()
 
@@ -127,31 +139,43 @@ class TestSpecMixin(ExecuteMixin, AssertionMixin):
         in_file_request = self.document.get(RequestConfigNode.ROOT) is not None
 
         keys = [TestSpecConfigNode.ROOT, ExecuteConfigNode.ROOT, ExecuteConfigNode.WITH]
-        out_file_with = dict_get(self.document, '.'.join(keys)) is not None
+        out_file_with = dict_get(self.document, ".".join(keys)) is not None
 
         if in_file_request and out_file_with:
-            raise SystemExit(err_message('fatal.V0021', extra={'spec': {'execute': {'with': 'Not allowed'}}}))
+            raise SystemExit(
+                err_message(
+                    "fatal.V0021", extra={"spec": {"execute": {"with": "Not allowed"}}}
+                )
+            )
 
 
 class TestcaseValueHandler:
     """
     Handle variables and values regarding testcase
     """
+
     @staticmethod
-    def request_set_result(execute_doc: dict, symbol_table: MappingProxyType, request_ret: MappingProxyType) -> dict:
+    def request_set_result(
+        execute_doc: dict, symbol_table: MappingProxyType, request_ret: MappingProxyType
+    ) -> dict:
         arg = [ExecuteConfigNode.ROOT, ExecuteConfigNode.RESULT]
-        result_replaceable = str(dict_get(execute_doc, '.'.join(arg))).strip()
-        
-        if result_replaceable.startswith('$'):  # if starts with $ remove it
+        result_replaceable = str(dict_get(execute_doc, ".".join(arg))).strip()
+
+        if result_replaceable.startswith("$"):  # if starts with $ remove it
             result_replaceable = result_replaceable[1:]
-        
-        if symbol_table.get(result_replaceable, '##') == '##':  # if key is non-existant
-            raise SystemExit(f'Variable `${result_replaceable}` was not declared')
-        
-        if request_ret.get('have_all'):
-            return {result_replaceable: {key: val for key, val in request_ret.items() if key != 'have_all'}}
+
+        if symbol_table.get(result_replaceable, "##") == "##":  # if key is non-existant
+            raise SystemExit(f"Variable `${result_replaceable}` was not declared")
+
+        if request_ret.get("have_all"):
+            return {
+                result_replaceable: {
+                    key: val for key, val in request_ret.items() if key != "have_all"
+                }
+            }
         else:
             for _, val in request_ret.items():
-                if val: return {result_replaceable: val}
+                if val:
+                    return {result_replaceable: val}
 
         return {}
