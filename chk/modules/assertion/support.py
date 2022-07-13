faec40886d8d629e3556ed280461569f9fc14826
HSN <hasanlock@gmail.com>
2022-07-13 13:22:19 +0600

    feature: start assertion process

--- chk/modules/assertion/support.py
+++ chk/modules/assertion/support.py
@@ -1,6 +1,22 @@
 """
 Assertion related services
 """
+from io import StringIO
+from unittest import TestCase, TestSuite, TextTestRunner
+
+
+class AssertionCase(TestCase):
+    def __init__(self, name: str, arg1, arg2):
+        super(AssertionCase, self).__init__(name)
+        self.type = type
+        self.arg1 = arg1
+        self.arg2 = arg2
+
+    def case_AssertEqual(self):
+        self.assertEqual(self.arg1, self.arg2)
+
+    def case_AssertDictEqual(self):
+        self.assertDictEqual(self.arg1, self.arg2)
 
 
 class AssertionHandler:
@@ -9,10 +25,16 @@ class AssertionHandler:
     """
 
     @staticmethod
-    def asserts_test_run(assertions: list) -> list:
+    def asserts_test_run(assertions: list):
         """
         Process given assertions and run test based on those
         :param assertions: list of passed assertions
         :return:
         """
-        pass
+        suite = TestSuite()
+
+        for each_assertion in assertions:
+            suite.addTest(AssertionCase(f"case_{each_assertion.type}", each_assertion.actual, each_assertion.expected))
+
+        run_result = TextTestRunner(stream=StringIO(), verbosity=0).run(suite)
+
