0655f9d835c4874919dca33d679de9edd71dad92
HSN <hasanlock@gmail.com>
2022-07-14 15:15:17 +0600

    refactor: add return type to replace_values
    - refactor: restructure format

--- chk/modules/variables/support.py
+++ chk/modules/variables/support.py
@@ -8,7 +8,10 @@ from chk.infrastructure.exception import err_message
 from chk.modules.http.constants import RequestConfigNode
 from chk.modules.http.support import RequestValueHandler
 from chk.modules.testcase.support import TestcaseValueHandler
-from chk.modules.variables.constants import VariableConfigNode as VarConf, LexicalAnalysisType
+from chk.modules.variables.constants import (
+    VariableConfigNode as VarConf,
+    LexicalAnalysisType,
+)
 from chk.modules.variables.validation_rules import variable_schema
 from chk.modules.version.constants import DocumentType
 from copy import deepcopy
@@ -16,13 +19,14 @@ from copy import deepcopy
 from chk.modules.variables.lexicon import StringLexicalAnalyzer
 
 
-def replace_values(doc: dict, var_s: dict):
+def replace_values(doc: dict, var_s: dict) -> dict[str, object]:
     """
     replace variables with values
     :param doc:
     :param var_s:
     :return:
     """
+
     for key in doc.keys():
         if type(doc[key]) is str:
             item = str(doc[key])
@@ -33,7 +37,7 @@ def replace_values(doc: dict, var_s: dict):
 
 
 class VariableMixin(object):
-    """ Mixin for variable spec. for v0.7.2"""
+    """Mixin for variable spec. for v0.7.2"""
 
     def __init__mixin__(self, symbol_tbl=None):
         """
@@ -50,35 +54,43 @@ class VariableMixin(object):
         try:
             request_doc = self.variable_as_dict()
             if not self.validator.validate(request_doc, variable_schema):
-                raise SystemExit(err_message('fatal.V0006', extra=self.validator.errors))
+                raise SystemExit(
+                    err_message("fatal.V0006", extra=self.validator.errors)
+                )
         except DocumentError as doc_err:
-            raise SystemExit(err_message('fatal.V0001', extra=doc_err)) from doc_err
+            raise SystemExit(err_message("fatal.V0001", extra=doc_err)) from doc_err
 
         return request_doc  # or is a success
 
     def variable_as_dict(self) -> dict[str, dict]:
         """Get variable dict"""
-        if not hasattr(self, 'validator') or not hasattr(self, 'document'):
-            raise SystemExit(err_message('fatal.V0005'))
+        if not hasattr(self, "validator") or not hasattr(self, "document"):
+            raise SystemExit(err_message("fatal.V0005"))
 
         try:
-            return {key: self.document[key] for key in (VarConf.ROOT,) if
-                    key in self.document}
+            return {
+                key: self.document[key]
+                for key in (VarConf.ROOT,)
+                if key in self.document
+            }
         except Exception as ex:
-            raise SystemExit(err_message('fatal.V0005', extra=ex))
+            raise SystemExit(err_message("fatal.V0005", extra=ex))
 
     def variable_process(self, la_type: LexicalAnalysisType) -> dict:
         self.__init__mixin__()
         return self.lexical_analysis_for(la_type, self.symbol_table)
 
     def build_symbol_table(self) -> dict:
-        """ Fill variable space"""
+        """Fill variable space"""
         doc = self.variable_as_dict().get(VarConf.ROOT, {})
-        if doc: return {key: doc[key] for key in doc.keys() if key in doc.keys()}
+        if doc:
+            return {key: doc[key] for key in doc.keys() if key in doc.keys()}
 
         return doc
 
-    def lexical_analysis_for(self, la_type: LexicalAnalysisType, symbol_table: dict) -> dict:
+    def lexical_analysis_for(
+        self, la_type: LexicalAnalysisType, symbol_table: dict
+    ) -> dict:
         """lexical validation"""
         document_part = {}
 
@@ -86,7 +98,11 @@ class VariableMixin(object):
             document_part = self.request_as_dict()
 
         document_replaced = deepcopy(self.document)
-        document_replaced[RequestConfigNode.ROOT] = RequestValueHandler.request_fill_val(document_part, symbol_table, replace_values)
+        document_replaced[
+            RequestConfigNode.ROOT
+        ] = RequestValueHandler.request_fill_val(
+            document_part, symbol_table, replace_values
+        )
 
         return document_replaced
 
@@ -108,13 +124,15 @@ class VariableMixin(object):
             return TestcaseValueHandler.request_set_result(
                 self.execute_as_dict(),
                 MappingProxyType(self.symbol_table),
-                MappingProxyType(request_ret)
+                MappingProxyType(request_ret),
             )
 
-        raise ValueError(f'variable_assemble_values: `{document_type}` not allowed')
+        raise ValueError(f"variable_assemble_values: `{document_type}` not allowed")
 
     @staticmethod
-    def variable_update_symbol_table(ctx_document: dict, updated: MappingProxyType) -> dict:
+    def variable_update_symbol_table(
+        ctx_document: dict, updated: MappingProxyType
+    ) -> dict:
         """
         Update symbol table and return updated document
         :param ctx_document:
