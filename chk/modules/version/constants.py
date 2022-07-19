cca7505acbf2b480134aa796094118980910b4a2
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-07-20 01:07:18 +0600

    [#110] refactor: removed VersionStrToSpecConfigMapping:find_by_version

--- chk/modules/version/constants.py
+++ chk/modules/version/constants.py
@@ -1,4 +1,3 @@
-from chk.infrastructure.exception import err_message
 from typing import Dict
 
 
@@ -13,14 +12,3 @@ class VersionStrToSpecConfigMapping:
     data: Dict = {
         "default:http:0.7.2": "chk.modules.http.entities.HttpSpec_V072",
     }
-
-    @classmethod
-    def find_by_version(cls, key: str) -> str:
-        """find config by version str"""
-        if not isinstance(key, str): raise SystemExit(err_message('fatal.V0004'))
-
-        class_map = cls.data.get(key)
-        if not isinstance(class_map, str): raise SystemExit(err_message('fatal.V0004'))
-        elif len(class_map) == 0: raise SystemExit(err_message('fatal.V0004'))
-
-        return class_map
