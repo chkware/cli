ab64319d69ef8a99880cd54870796a433f0289e2
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-07-20 01:06:28 +0600

    [#110] refactor: removed chk.modules.version.support.SpecificationLoader

--- chk/modules/version/support.py
+++ chk/modules/version/support.py
@@ -1,33 +1,12 @@
 """
 version related support services
 """
-import pydoc
 
 from cerberus import validator
+
 from chk.infrastructure.exception import err_message
-from chk.infrastructure.file_loader import FileContext
-from chk.modules.version.constants import VersionConfigNode, VersionStrToSpecConfigMapping as Mapping
+from chk.modules.version.constants import VersionConfigNode
 from chk.modules.version.validation_rules import version_schema
-
-
-class SpecificationLoader:
-    """Specification file loading service"""
-
-    @classmethod
-    def to_spec_config(cls, file_ctx: FileContext):
-        """Creates and returns AbstractSpecConfig from version string"""
-        class_map = Mapping.find_by_version(cls.version(file_ctx.document))
-        http_config_class = pydoc.locate(class_map)
-
-        if not callable(http_config_class):
-            raise SystemExit(err_message('fatal.V0007'))
-
-        return http_config_class(file_ctx)
-
-    @classmethod
-    def version(cls, document) -> str:
-        """check and get version string"""
-        return str(document.get(VersionConfigNode.VERSION))
 
 
 class VersionMixin(object):
@@ -50,6 +29,7 @@ class VersionMixin(object):
             raise SystemExit(err_message('fatal.V0005'))
 
         try:
-            return {key:self.document[key] for key in (VersionConfigNode.VERSION, ) if key in self.document}  # type: ignore
+            return {key: self.document[key] for key in (VersionConfigNode.VERSION,) if
+                    key in self.document}  # type: ignore
         except Exception as ex:
             raise SystemExit(err_message('fatal.V0005', extra=ex))
