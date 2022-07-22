7748a629d6cad84b991608b541c8d5bff87e0140
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-07-22 15:56:04 +0600

    [#114] feature: Presentation class implemented for handling display of result of chkware commands

--- chk/modules/http/presentation.py
+++ chk/modules/http/presentation.py
@@ -3,23 +3,28 @@ Presentation related logics
 """
 
 
-def make_displayable(response: dict[str, object]) -> str:
-    def headers(res) -> str:
-        """Headers"""
-        return '{}'.format(
-            '\r\n'.join('{}: {}'.format(k, v) for k, v in res.get('headers').items()),
-        )
+class Presentation:
+    """Handle presentation of the outputs of chkware commands."""
 
-    if response.get('have_all'):
-        summary = '{} {} {}'.format(
-            response.get('version'),
-            response.get('code'),
-            response.get('reason'),
-        )
+    @staticmethod
+    def display_result(response: dict[str, object]) -> str:
+        """Display response in presentable format."""
+        def headers(res) -> str:
+            """Headers"""
+            return '{}'.format(
+                '\r\n'.join('{}: {}'.format(k, v) for k, v in res.get('headers').items()),
+            )
 
-        return '{}\r\n{}\r\n\r\n{}'.format(summary, headers(response), response.get('body'))
-    else:
-        response.pop('have_all')
-        for _, val in response.items():
-            if val:
-                return str(val)
+        if response.get('have_all'):
+            summary = '{} {} {}'.format(
+                response.get('version'),
+                response.get('code'),
+                response.get('reason'),
+            )
+
+            return '{}\r\n{}\r\n\r\n{}'.format(summary, headers(response), response.get('body'))
+        else:
+            response.pop('have_all')
+            for _, val in response.items():
+                if val:
+                    return str(val)
