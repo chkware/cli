daf861eeabb956b1c895d2291d2a37197a73720d
HSN <hasanlock@gmail.com>
2022-07-24 17:28:25 +0600

    feature: different validation rules by version

--- chk/modules/version/validation_rules.py
+++ chk/modules/version/validation_rules.py
@@ -3,11 +3,20 @@ Validation rules and supporting libs for version module
 """
 from chk.modules.version.constants import VersionStore
 
-version_schema = {
+version_schema_http = {
     'version': {
         'required': True,
         'type': 'string',
         'empty': False,
-        'allowed': VersionStore.request_versions + VersionStore.testcase_versions
+        'allowed': VersionStore.request_versions
+    }
+}
+
+version_schema_testcase = {
+    'version': {
+        'required': True,
+        'type': 'string',
+        'empty': False,
+        'allowed': VersionStore.testcase_versions
     }
 }
