5f5be676448aa4f559d3c70b3bd09fd779a7adf3
HSN <hasanlock@gmail.com>
2022-07-27 23:09:42 +0600

    feature: data_get function

--- chk/console/helper.py
+++ chk/console/helper.py
@@ -32,7 +32,7 @@ def dict_set(var: dict, keymap: str, value: object) -> bool:
     Set a value of a dictionary by dot notation key and given value
     :param var: the dictionary we'll get value for
     :param keymap: dot separated keys
-    :param value:  
+    :param value:
     :return:
     """
 
@@ -53,3 +53,31 @@ def dict_set(var: dict, keymap: str, value: object) -> bool:
             return True
     else:
         return False
+
+
+def data_get(var: dict|list, keymap: str, default=None) -> object:
+    """
+    Get a value of a dictionary|list by dot notation key
+    :param var: the dictionary|list we'll get value for
+    :param keymap: dot separated keys
+    :param default: None
+    :return:
+    """
+    if len(keymap) == 0: return default
+    elif not var: return default
+
+    dot_loc = keymap.find('.')
+
+    if dot_loc < 1: key = keymap
+    else: key = keymap[:keymap.find('.')]
+
+    if key.isnumeric(): key = int(key)
+
+    if dot_loc < 1: key_last = None
+    else: key_last = keymap[(keymap.find('.') + 1):]
+
+    try:
+        if key_last is None: return var[key]
+        else: return data_get(var[key], key_last, default)
+    except LookupError:
+        return default
