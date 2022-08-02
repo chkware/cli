089164e0af8691aa488c67efa0d431dab0d9e16f
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-02 23:45:53 +0600

    [#113] feature: new parameter options added in the http.execute for handling boolean command line flags.

--- chk/modules/http/main.py
+++ chk/modules/http/main.py
@@ -1,22 +1,23 @@
 """
 main driver
 """
+
 from chk.infrastructure.file_loader import ChkFileLoader, FileContext
 from chk.infrastructure.work import handle_worker
 from chk.modules.http.entities import HttpSpec
 from chk.modules.http.presentation import Presentation
 
 
-def execute(file: str, result: bool):
+def execute(file: str, options: dict[str, bool]):
     """execute command"""
     ChkFileLoader.is_file_ok(file)
     document = ChkFileLoader.to_dict(file)
     fpath_mangled, fpath_hash = ChkFileLoader.get_mangled_name(file)
 
-    file_ctx = FileContext(file, fpath_mangled, fpath_hash, document)
+    file_ctx = FileContext(file, fpath_mangled, fpath_hash, document, options)
     http_spec = HttpSpec(file_ctx)
     try:
         response = handle_worker(http_spec)
     except BaseException as error:
         response = error
-    Presentation.present_result(file_ctx, response, result)
+    Presentation.present_result(file_ctx, response)
