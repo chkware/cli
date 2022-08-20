847c0d7d1c442d92abd59a40b4961a7a86f93cbe
Sudipto Baral <sudiptobaral.me@gmail.com>
2022-08-20 13:22:48 +0600

    [#116] test: AssertMapContains unittests added.

--- tests/modules/assertion/assertion_support_test.py
+++ tests/modules/assertion/assertion_support_test.py
@@ -35,3 +35,33 @@ class TestAssertionCase:
         )
         with pytest.raises(AssertionError):
             assertion_case.case_AssertListContains()
+
+    def test_assert_map_contains_expect_not_exist(self):
+        """Tests AssertMapContains when expect is not in actual."""
+        name = 'AssertMapContains'
+        name_run = 'AssertMapContains_72a688341d4611edb365ebb9b969d060'
+        actual = {'a': 1, 'b': 2}
+        expect = 'd'
+        assertion_case = AssertionCase(
+            name=name,
+            name_run=name_run,
+            actual=actual,
+            expect=expect
+        )
+        with pytest.raises(AssertionError):
+            assertion_case.case_AssertMapContains()
+
+    def test_assert_map_contains_actual_not_map(self):
+        """Tests AsserMapContains when actual is not a map."""
+        name = 'AssertMapContains'
+        name_run = 'AssertMapContains_72a688341d4611edb365ebb9b969d060'
+        actual = [1, 2, 3]
+        expect = 3
+        assertion_case = AssertionCase(
+            name=name,
+            name_run=name_run,
+            actual=actual,
+            expect=expect
+        )
+        with pytest.raises(AssertionError):
+            assertion_case.case_AssertMapContains()
