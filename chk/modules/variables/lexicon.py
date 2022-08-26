f581f2556c1aa6233ff8a991585aac6a59f22d2b
HSN <hasanlock@gmail.com>
2022-08-26 16:53:55 +0600

    refactor: improve replace_in_str impl
    closes #127

--- chk/modules/variables/lexicon.py
+++ chk/modules/variables/lexicon.py
@@ -14,10 +14,11 @@ class StringLexicalAnalyzer:
         if type(container) is not str: raise TypeError
         if len(replace_with) == 0: return container
 
-        line_split = re.split("{|}", container)
-        line_strip = [item.strip() for item in line_split if item]
+        line_split = re.split(r'({\s*\$\w\s*})', container)
+        line_strip = [''.join(item.split()) for item in line_split if item]
 
         for i, item in enumerate(line_strip):
+            item = item.strip('{}')
             if item.startswith('$'):
                 value = data_get(replace_with, item.lstrip('$'), None)
                 line_strip[i] = str(value) if value else '{' + item + '}'
