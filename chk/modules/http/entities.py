75cea360a6926bb17334da9ed8baea0f3c50b0b0
HSN <hasanlock@gmail.com>
2022-09-03 16:25:08 +0600

    feat(http): prepare default document

--- chk/modules/http/entities.py
+++ chk/modules/http/entities.py
@@ -3,6 +3,7 @@ Entities for http document specification
 """
 
 from cerberus import Validator
+
 from chk.infrastructure.file_loader import FileContext
 from chk.infrastructure.work import WorkerContract, RequestProcessorContract, handle_request
 from chk.modules.http.request_helper import RequestProcessorMixin_PyRequests
@@ -12,6 +13,9 @@ from chk.modules.variables.support import VariableMixin
 from chk.modules.version.support import VersionMixin
 from chk.modules.variables.constants import LexicalAnalysisType
 from chk.modules.version.constants import DocumentType
+
+from chk.modules.variables.entities import get_returnable_variable_doc_spec
+from chk.modules.version.entities import get_version_doc_spec
 
 __request_document_specification = {
     RConst.ROOT: {
@@ -50,6 +54,9 @@ class HttpSpec(
     WorkerContract,
     RequestProcessorContract
 ):
+    """
+    Holds http specification activity
+    """
 
     def __init__(self, file_ctx: FileContext):
         self.file_ctx, self.document, self.validator = file_ctx, file_ctx.document, Validator()
@@ -62,3 +69,11 @@ class HttpSpec(
         ctx_document = self.variable_process(LexicalAnalysisType.REQUEST)
         out_response = handle_request(self, ctx_document)
         return self.variable_assemble_values(ctx_document, out_response)
+
+    @staticmethod
+    def get_default_http_doc() -> dict:
+        """
+        Get a http document specification with default values set
+        """
+        request_dict = get_request_doc_spec()[RConst.ROOT] | get_returnable_variable_doc_spec()
+        return get_version_doc_spec() | {RConst.ROOT: request_dict}
