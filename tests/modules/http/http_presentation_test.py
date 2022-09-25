a8516169acc892c69795a57fa697c7233fa70d28
HSN <hasanlock@gmail.com>
2022-09-25 12:19:11 +0600

    refactor(tests): http presentation tests

--- tests/modules/http/http_presentation_test.py
+++ tests/modules/http/http_presentation_test.py
@@ -10,14 +10,14 @@ class TestHttpPresentation:
     def test_displayable_summary_success(self):
         """Test test_displayable_summary with success parameter."""
         filepath = '/home/some_path/GET-WithHeaders.chk'
-        expected_summary = 'File: {}\r\n\nExecuting request\r\n\n- Making request [Success]\r\n===='.format(filepath)
+        expected_summary = f'File: {filepath}\r\n\nExecuting request\r\n\n- Making request [Success]\r\n===='
         summary = Presentation.displayable_summary(filepath, status='Success')
         assert expected_summary == summary
 
     def test_displayable_summary_failed(self):
         """Test test_displayable_summary with failed parameter."""
         filepath = '/home/some_path/GET-WithHeaders.chk'
-        expected_summary = 'File: {}\r\n\nExecuting request\r\n\n- Making request [Failed]\r\n===='.format(filepath)
+        expected_summary = f'File: {filepath}\r\n\nExecuting request\r\n\n- Making request [Failed]\r\n===='
         summary = Presentation.displayable_summary(filepath, status='Failed')
         assert expected_summary == summary
 
@@ -28,7 +28,7 @@ class TestHttpPresentation:
                 result=True,
             ),
         )
-        file_ctx = FileContext(None, None, None, None, options)
+        file_ctx = FileContext(options)
         response = {'have_all': False}
         Presentation.present_result(file_ctx, response)
         captured = capsys.readouterr()
@@ -41,7 +41,7 @@ class TestHttpPresentation:
                 result=False,
             ),
         )
-        file_ctx = FileContext(None, None, None, None, options)
+        file_ctx = FileContext(options)
         response = {'have_all': False}
         Presentation.present_result(file_ctx, response)
         captured = capsys.readouterr()
