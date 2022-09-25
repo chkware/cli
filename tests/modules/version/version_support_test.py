6b3a98ee0ecd916f3fc232d7efbd75925b2fcf80
HSN <hasanlock@gmail.com>
2022-09-25 14:23:09 +0600

    refactor(tests): requests and version tests

--- tests/modules/version/version_support_test.py
+++ tests/modules/version/version_support_test.py
@@ -1,44 +1,61 @@
 import pytest
 
-from cerberus import Validator
+from chk.infrastructure.contexts import app
+from chk.infrastructure.file_loader import FileContext
 from chk.modules.version.support import VersionMixin
 from chk.modules.version.constants import DocumentType
 
 
 class HavingVersion(VersionMixin):
-    def __init__(self) -> None:
-        self.document, self.validator = None, Validator()        
+    def __init__(self, file_ctx: FileContext) -> None:
+        self.file_ctx = file_ctx
+
+    def get_file_context(self) -> FileContext:
+        return self.file_ctx
 
 
 class TestVersionMixin:
     def test_validate_config_success(self):
-        ver = HavingVersion()
-        ver.document = {'version': 'default:http:0.7.2'}
-        assert type(ver.version_validated(DocumentType.HTTP)) is dict
+        file_ctx = FileContext(filepath_hash='a1b2')
+        app.original_doc[file_ctx.filepath_hash] = \
+            {'version': 'default:http:0.7.2'}
+
+        ver = HavingVersion(file_ctx)
+        assert isinstance(ver.version_validated(), dict)
         
     def test_validate_config_empty_version(self):
-        ver = HavingVersion()
+        file_ctx = FileContext(filepath_hash='a1b2')
+        app.original_doc[file_ctx.filepath_hash] = \
+            {'version': ''}
+
+        ver = HavingVersion(file_ctx)
         with pytest.raises(SystemExit):
-            ver.document = {'version': ''}
-            ver.version_validated(DocumentType.HTTP)
+            ver.version_validated()
 
     def test_validate_config_fail_non_exist_ver(self):
-        ver = HavingVersion()
+        file_ctx = FileContext(filepath_hash='a1b2')
+        app.original_doc[file_ctx.filepath_hash] = \
+            {'version': 'default:http:0.7'}
+
+        ver = HavingVersion(file_ctx)
         with pytest.raises(SystemExit):
-            ver.document = {'version': 'default:http:0.7'}
-            ver.version_validated(DocumentType.HTTP)
+            ver.version_validated()
             
     def test_validate_config_fail_no_doc(self):
-        ver = HavingVersion()
+        file_ctx = FileContext(filepath_hash='a1b2')
+        app.original_doc[file_ctx.filepath_hash] = {}
+
+        ver = HavingVersion(file_ctx)
         with pytest.raises(SystemExit):
-            ver.document = {}
-            ver.version_validated(DocumentType.HTTP)
+            ver.version_validated()
 
     def test_validate_config_fail_on_none(self):
-        ver = HavingVersion()
+        file_ctx = FileContext(filepath_hash='a1b2')
+        app.original_doc[file_ctx.filepath_hash] = None
+
+        ver = HavingVersion(file_ctx)
         with pytest.raises(SystemExit):
-            ver.document = None
-            ver.version_validated(DocumentType.HTTP)
+            ver.version_validated()
             
     def test_validate_config_fail_no_version(self):
         config = {
@@ -47,8 +64,10 @@ class TestVersionMixin:
             }
         }
 
-        ver = HavingVersion()
-        with pytest.raises(SystemExit):
-            ver.document = config
-            ver.version_validated(DocumentType.HTTP)
+        file_ctx = FileContext(filepath_hash='a1b2')
+        app.original_doc[file_ctx.filepath_hash] = config
 
+
+        ver = HavingVersion(file_ctx)
+        with pytest.raises(SystemExit):
+            ver.version_validated()
