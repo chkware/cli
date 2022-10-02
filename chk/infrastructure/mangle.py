8dc19ec9964f4ae7254a51651fd1bfc5442c2cef
HSN <hasanlock@gmail.com>
2022-10-02 14:48:24 +0600

    refactor(infra): move imports for top

--- chk/infrastructure/mangle.py
+++ chk/infrastructure/mangle.py
@@ -1,12 +1,13 @@
 """
 mangle library
 """
+import hashlib
+import pathlib
 
 
 def filename(file_name: str) -> str:
     """Mangle filepath to filename string"""
 
-    import pathlib
     path = pathlib.Path(file_name)
     m_file = str(path.resolve())
     return m_file.lower().strip('/').replace('/', '-').replace('\\', '_').replace(':', '~')
@@ -14,5 +15,5 @@ def filename(file_name: str) -> str:
 
 def uniq_sha255(hashable: str) -> str:
     """Convert a string to hash"""
-    import hashlib
+
     return hashlib.sha256(hashable.encode('utf-8')).hexdigest()
