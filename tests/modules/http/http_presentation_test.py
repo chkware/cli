81f8bfe7fd4ac1c8814d7a4ec170fd18f63c2099
HSN <hasanlock@gmail.com>
2022-10-11 12:30:15 +0600

    fix: TestHttpPresentation::test_displayable_summary_success

--- tests/modules/http/http_presentation_test.py
+++ tests/modules/http/http_presentation_test.py
@@ -1,7 +1,8 @@
 from types import MappingProxyType
 
+from chk.infrastructure.contexts import app
 from chk.infrastructure.file_loader import FileContext
-from chk.modules.http.presentation import Presentation
+from chk.modules.http.presentation import Presentation, buffer_msg
 
 
 class TestHttpPresentation:
@@ -9,10 +10,12 @@ class TestHttpPresentation:
 
     def test_displayable_summary_success(self):
         """Test test_displayable_summary with success parameter."""
-        filepath = '/home/some_path/GET-WithHeaders.chk'
-        expected_summary = f'File: {filepath}\r\n\nExecuting request\r\n\n- Making request [Success]\r\n===='
-        summary = Presentation.displayable_summary(filepath, status='Success')
-        assert expected_summary == summary
+        app.config("buffer_access_off", False)
+
+        expected_summary = 'Some test case\r\n\r\n===='
+        buffer_msg('Some test case')
+
+        assert expected_summary == Presentation.displayable_summary()
 
     def test_displayable_summary_failed(self):
         """Test test_displayable_summary with failed parameter."""
