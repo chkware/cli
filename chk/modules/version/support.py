0ab68cbe5325a2945c226786a504e625c767a368
HSN <hasanlock@gmail.com>
2022-10-14 14:51:35 +0600

    feat(version): read a spec file to get version string

--- chk/modules/version/support.py
+++ chk/modules/version/support.py
@@ -10,52 +10,89 @@ from chk.infrastructure.contexts import validator, app
 from chk.infrastructure.file_loader import FileContext
 
 from chk.modules.version.constants import VersionConfigNode, DocumentType
-from chk.modules.version.validation_rules import version_schema_http, version_schema_testcase
+from chk.modules.version.validation_rules import (
+    version_schema_http,
+    version_schema_testcase,
+)
 
 
 class VersionMixin:
-    """ Mixin for version spec. for v0.7.2"""
+    """Mixin for version spec. for v0.7.2"""
 
     @abc.abstractmethod
     def get_file_context(self) -> FileContext:
-        """ Abstract method to get file context """
+        """Abstract method to get file context"""
 
     def version_validated(self) -> dict[str, str]:
-        """ Validate the schema against config """
+        """Validate the schema against config"""
 
         try:
             version_doc = self.version_as_dict()
             version_schema = {}
 
             match self.get_document_type():
-                case DocumentType.HTTP: version_schema = version_schema_http
-                case DocumentType.TESTCASE: version_schema = version_schema_testcase
+                case DocumentType.HTTP:
+                    version_schema = version_schema_http
+                case DocumentType.TESTCASE:
+                    version_schema = version_schema_testcase
 
             if not validator.validate(version_doc, version_schema):
-                raise SystemExit(err_message('fatal.V0006', extra=validator.errors))
+                raise SystemExit(err_message("fatal.V0006", extra=validator.errors))
         except DocumentError as doc_err:
-            raise SystemExit(err_message('fatal.V0001', extra=doc_err)) from doc_err
+            raise SystemExit(err_message("fatal.V0001", extra=doc_err)) from doc_err
 
         return version_doc  # or is a success
 
     def version_as_dict(self) -> dict[str, str]:
-        """ Get version as dictionary """
+        """Get version as dictionary"""
 
         file_ctx = self.get_file_context()
         document = app.get_original_doc(file_ctx.filepath_hash)
 
         try:
-            return {key: document[key] for key in (VersionConfigNode.VERSION,) if key in document}
+            return {
+                key: document[key]
+                for key in (VersionConfigNode.VERSION,)
+                if key in document
+            }
         except Exception as ex:
-            raise SystemExit(err_message('fatal.V0005', extra=ex)) from ex
+            raise SystemExit(err_message("fatal.V0005", extra=ex)) from ex
 
     def get_document_type(self) -> DocumentType:
-        """ Get document type """
+        """Get document type"""
 
         version_doc = self.version_as_dict().get(VersionConfigNode.VERSION)
-        version_doc_l = str(version_doc).split(':')
+        version_doc_l = str(version_doc).split(":")
 
         if len(version_doc_l) < 2:
-            raise SystemExit('get_document_type: Invalid version type')
+            raise SystemExit("get_document_type: Invalid version type")
 
         return DocumentType.from_value(version_doc_l.pop(1))
+
+
+class RawFileVersionParser:
+    """Utility to parse version string from raw file"""
+
+    @staticmethod
+    def find_version_str(file: str) -> str:
+        """Read a document in raw format, and find version string"""
+
+        try:
+            with open(file, encoding="UTF-8") as file_handle:
+                file_content = file_handle.readlines()
+                version_doc = next(
+                    each_line
+                    for each_line in file_content
+                    if each_line.startswith("version:")
+                )
+
+                version_doc = version_doc.strip(" \r\n")
+                version_parts = [item.strip(" '") for item in version_doc.split(":", 1)]
+
+                if len(version_parts) < 2:
+                    return ""
+
+                return version_parts[1]
+
+        except (FileNotFoundError, StopIteration):
+            return ""
--- tests/modules/version/version_support_test.py
+++ tests/modules/version/version_support_test.py
@@ -1,9 +1,9 @@
 import pytest
+import tests
 
 from chk.infrastructure.contexts import app
 from chk.infrastructure.file_loader import FileContext
-from chk.modules.version.support import VersionMixin
-from chk.modules.version.constants import DocumentType
+from chk.modules.version.support import VersionMixin, RawFileVersionParser
 
 
 class HavingVersion(VersionMixin):
@@ -16,33 +16,30 @@ class HavingVersion(VersionMixin):
 
 class TestVersionMixin:
     def test_validate_config_success(self):
-        file_ctx = FileContext(filepath_hash='a1b2')
-        app.original_doc[file_ctx.filepath_hash] = \
-            {'version': 'default:http:0.7.2'}
+        file_ctx = FileContext(filepath_hash="a1b2")
+        app.original_doc[file_ctx.filepath_hash] = {"version": "default:http:0.7.2"}
 
         ver = HavingVersion(file_ctx)
         assert isinstance(ver.version_validated(), dict)
-        
+
     def test_validate_config_empty_version(self):
-        file_ctx = FileContext(filepath_hash='a1b2')
-        app.original_doc[file_ctx.filepath_hash] = \
-            {'version': ''}
+        file_ctx = FileContext(filepath_hash="a1b2")
+        app.original_doc[file_ctx.filepath_hash] = {"version": ""}
 
         ver = HavingVersion(file_ctx)
         with pytest.raises(SystemExit):
             ver.version_validated()
 
     def test_validate_config_fail_non_exist_ver(self):
-        file_ctx = FileContext(filepath_hash='a1b2')
-        app.original_doc[file_ctx.filepath_hash] = \
-            {'version': 'default:http:0.7'}
+        file_ctx = FileContext(filepath_hash="a1b2")
+        app.original_doc[file_ctx.filepath_hash] = {"version": "default:http:0.7"}
 
         ver = HavingVersion(file_ctx)
         with pytest.raises(SystemExit):
             ver.version_validated()
-            
+
     def test_validate_config_fail_no_doc(self):
-        file_ctx = FileContext(filepath_hash='a1b2')
+        file_ctx = FileContext(filepath_hash="a1b2")
         app.original_doc[file_ctx.filepath_hash] = {}
 
         ver = HavingVersion(file_ctx)
@@ -50,24 +47,37 @@ class TestVersionMixin:
             ver.version_validated()
 
     def test_validate_config_fail_on_none(self):
-        file_ctx = FileContext(filepath_hash='a1b2')
+        file_ctx = FileContext(filepath_hash="a1b2")
         app.original_doc[file_ctx.filepath_hash] = None
 
         ver = HavingVersion(file_ctx)
         with pytest.raises(SystemExit):
             ver.version_validated()
-            
+
     def test_validate_config_fail_no_version(self):
-        config = {
-            'request': {
-                'url': 'some.url'
-            }
-        }
+        config = {"request": {"url": "some.url"}}
 
-        file_ctx = FileContext(filepath_hash='a1b2')
+        file_ctx = FileContext(filepath_hash="a1b2")
         app.original_doc[file_ctx.filepath_hash] = config
-
 
         ver = HavingVersion(file_ctx)
         with pytest.raises(SystemExit):
             ver.version_validated()
+
+
+class TestRawFileVersionParser:
+    def test_find_version_str_success(self):
+        file = tests.RES_DIR + "UserOk.chk"
+        assert RawFileVersionParser.find_version_str(file) == "default:http:0.7.2"
+
+    def test_find_version_str_fails_when_no_version_found(self):
+        file = tests.RES_DIR + "UserNoVersion.chk"
+        assert not RawFileVersionParser.find_version_str(file)
+
+    def test_find_version_str_fails_when_commented_version_found(self):
+        file = tests.RES_DIR + "UserCommentedVersion.chk"
+        assert not RawFileVersionParser.find_version_str(file)
+
+    def test_find_version_str_fails_when_file_not_found(self):
+        file = tests.RES_DIR + "UserCommentedVersions.chk"
+        assert not RawFileVersionParser.find_version_str(file)
