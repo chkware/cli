2e72f6e91133e54b7f28f7855940e8ea44b47326
HSN <hasanlock@gmail.com>
2022-10-20 21:24:41 +0600

    feat(lexicon): allow combined string value replace

--- chk/modules/variables/lexicon.py
+++ chk/modules/variables/lexicon.py
@@ -17,13 +17,13 @@ class StringLexicalAnalyzer:
         if len(replace_with) == 0:
             return container
 
-        line_split = re.split(r'({\s*\$\w+\s*})', container)
-        line_strip = [''.join(item.split()) for item in line_split if item]
+        line_split = re.split(r"({\s*\$\w+\s*})|(\s*\$\w+)", container)
+        line_strip = ["".join(item.split()) for item in line_split if item]
 
         for i, item in enumerate(line_strip):
-            item = item.strip('{}')
-            if item.startswith('$'):
-                value = data_get(replace_with, item.lstrip('$'), None)
-                line_strip[i] = str(value) if value else '{' + item + '}'
+            item = item.strip("{}")
+            if item.startswith("$"):
+                value = data_get(replace_with, item.lstrip("$"), None)
+                line_strip[i] = str(value) if value else "{" + item + "}"
 
-        return ''.join(line_strip)
+        return "".join(line_strip)
