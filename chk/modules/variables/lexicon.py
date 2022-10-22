665eb5de3b6a304ceca043b4cbbe3122a53dd804
HSN <hasanlock@gmail.com>
2022-10-22 13:10:02 +0600

    feat(lexicon): replace value in variable string

--- chk/modules/variables/lexicon.py
+++ chk/modules/variables/lexicon.py
@@ -27,3 +27,32 @@ class StringLexicalAnalyzer:
                 line_strip[i] = str(value) if value else "{" + item + "}"
 
         return "".join(line_strip)
+
+    @staticmethod
+    def replace(container: str, replace_with: dict) -> object:
+        """replace values found in string with typed return"""
+
+        if not isinstance(container, str):
+            raise TypeError
+
+        if len(replace_with) == 0:
+            return container
+
+        line_split = re.split(
+            r"({\s*\$[a-zA-Z0-9_.]+\s*})|(\s*\$[a-zA-Z0-9_.]+)", container
+        )
+        line_strip = ["".join(item.split()) for item in line_split if item]
+
+        if len(line_strip) == 1:
+            item = line_strip.pop()
+            t_item = item.strip("{}").lstrip("$")
+            value = data_get(replace_with, t_item)
+            return value if value else "{ $" + t_item + " }"
+
+        for i, item in enumerate(line_strip):
+            item = item.strip("{}")
+            if item.startswith("$"):
+                value = data_get(replace_with, item.lstrip("$"), None)
+                line_strip[i] = str(value) if value else "{" + item + "}"
+
+        return "".join(line_strip)
--- tests/modules/variables/string_lexical_analyzer_test.py
+++ tests/modules/variables/string_lexical_analyzer_test.py
@@ -48,3 +48,57 @@ def test_replace_in_str_when_combined_pass():
     # print(StringLexicalAnalyzer.replace_in_str(container, replace_with))
 
     assert "1:|my name" == StringLexicalAnalyzer.replace_in_str(container, replace_with)
+
+
+class TestStringLexicalAnalyzerReplace:
+    @staticmethod
+    def test_replace_pass_when_single_variable():
+        replace_with = {"var1": 1, "var_3": "my name"}
+
+        container = "$var1"
+        assert 1 == StringLexicalAnalyzer.replace(container, replace_with)
+
+        container = "$var2"
+        assert "{ $var2 }" == StringLexicalAnalyzer.replace(container, replace_with)
+
+        replace_with = {"var1": [1, 2], "var_3": "my name"}
+
+        container = "{$var1}"
+        assert [1, 2] == StringLexicalAnalyzer.replace(container, replace_with)
+
+        container = "{ $var1    }"
+        assert [1, 2] == StringLexicalAnalyzer.replace(container, replace_with)
+
+        container = "{$var2}"
+        assert "{ $var2 }" == StringLexicalAnalyzer.replace(container, replace_with)
+
+    @staticmethod
+    def test_replace_pass_when_multiple_variable():
+        container = "$var1:$var_3"
+
+        replace_with = {"var1": 2, "var_3": "my_name"}
+        assert "2:my_name" == StringLexicalAnalyzer.replace(container, replace_with)
+
+        replace_with = {"var1": [1, 2], "var_3": "my_name"}
+        assert "[1, 2]:my_name" == StringLexicalAnalyzer.replace(
+            container, replace_with
+        )
+
+        container = "$var1:$var_3:$var4"
+        replace_with = {"var1": [1, 2], "var_3": "my_name", "var4": {"a": 1}}
+        assert "[1, 2]:my_name:{'a': 1}" == StringLexicalAnalyzer.replace(
+            container, replace_with
+        )
+
+    @staticmethod
+    def test_replace_pass_when_multiple_variable_with_curly_braces():
+        replace_with = {"var1": 2, "var_3": "my_name"}
+
+        container = "$var1:{$var_3}"
+        assert "2:my_name" == StringLexicalAnalyzer.replace(container, replace_with)
+
+        container = "{$var1}:{$var_3}"
+        assert "2:my_name" == StringLexicalAnalyzer.replace(container, replace_with)
+
+        container = "{$var1}:{{$var_3}}"
+        assert "2:{my_name}" == StringLexicalAnalyzer.replace(container, replace_with)
