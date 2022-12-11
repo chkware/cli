589834c3ec9ffce2560190e41a2614a64c06e096
HSN <hasanlock@gmail.com>
2022-12-11 11:20:09 +0600

    feat: test for helper.type_converter for dict and list in string

--- tests/console/helper_test.py
+++ tests/console/helper_test.py
@@ -224,3 +224,17 @@ class TestTypeConverter:
 
         a = "None"
         assert type_converter(a) is None
+
+    def test_type_converter_list_pass(self):
+        a = "['g', 1, 3]"
+        assert type_converter(a) == ["g", 1, 3]
+
+        a = "['a', 1, 3, [3, 4]]"
+        assert type_converter(a) == ["a", 1, 3, [3, 4]]
+
+    def test_type_converter_dict_pass(self):
+        a = "{'g': 23, 'f': 3}"
+        assert type_converter(a) == {"g": 23, "f": 3}
+
+        a = "{'g': 23, 'f': 3, 't': [1, 2]}"
+        assert type_converter(a) == {"g": 23, "f": 3, "t": [1, 2]}
