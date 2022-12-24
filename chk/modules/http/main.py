eee8e794b7e09d3a453a7617d474cbb28aae10eb
HSN <hasanlock@gmail.com>
2022-12-24 11:45:00 +0600

    feat: formatted printing for http

--- chk/modules/http/main.py
+++ chk/modules/http/main.py
@@ -1,6 +1,10 @@
 """
 Main driver
 """
+import sys
+from traceback import print_exc
+from typing import Any
+
 from chk.infrastructure.contexts import app
 from chk.infrastructure.work import handle_worker
 from chk.infrastructure.file_loader import FileContext
@@ -8,8 +12,35 @@ from chk.infrastructure.file_loader import FileContext
 from chk.modules.http.entities import HttpSpec
 
 
-def present_result(exposable: list) -> str:
-    return "\r\n\r\n".join([str(item) for item in exposable])
+def present_result(exposable: Any) -> str:
+    """Present result"""
+
+    def pprint(sections: dict) -> str:
+        resp = ""
+
+        if "version" in sections and "code" in sections and "reason" in sections:
+            resp += f'{sections["version"]} {sections["code"]} {sections["reason"]}'
+
+            resp += "\r\n\r\n"
+
+        if "headers" in sections:
+            items = sections["headers"].items()
+            resp += "\r\n".join(f"{k}: {v}" for k, v in items)
+
+            resp += "\r\n\r\n"
+
+        if "body" in sections:
+            b = sections["body"]
+            resp += str(b) if not isinstance(b, str) else b
+
+        if not resp:
+            resp = str(sections)
+
+        return resp
+
+    return "\r\n\r\n".join(
+        [pprint(item) if isinstance(item, dict) else str(item) for item in exposable]
+    )
 
 
 def execute(file_ctx: FileContext) -> None:
@@ -19,6 +50,5 @@ def execute(file_ctx: FileContext) -> None:
     try:
         response = handle_worker(http_spec)
         app.print_fmt(response, present_result)
-    except BaseException as error:
-        app.print_fmt("\r\n---", ret_s=bool(app.config("buffer_access_off")))
-        app.print_fmt(error)
+    except BaseException:
+        print_exc(file=sys.stderr)
