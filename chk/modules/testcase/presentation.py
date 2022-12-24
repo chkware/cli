45f8f0792fa67eede0c8bd734159935e3c015568
HSN <hasanlock@gmail.com>
2022-12-25 00:15:16 +0600

    feat: testcase presentation formatter

--- chk/modules/testcase/presentation.py
+++ chk/modules/testcase/presentation.py
@@ -4,7 +4,7 @@ Presenting assert data
 from typing import TypeAlias
 from dataclasses import dataclass
 
-from chk.infrastructure.file_loader import FileContext
+from chk.modules.http.presentation import present_dict
 
 
 @dataclass
@@ -24,22 +24,61 @@ class AssertResult:
 AssertResultList: TypeAlias = list[AssertResult]
 
 
-class Presentation:
-    @staticmethod
-    def displayable_file_info(file_ctx: FileContext) -> str:
-        return f"File: {file_ctx.filepath}\n"
+# assertion_results = app.get_local(self.file_ctx.filepath_hash, AConst.LOCAL)
+#
+# if not isinstance(assertion_results, list):
+#     raise RuntimeError
+#
+# for assertion_result in assertion_results:
+#     print(
+#         Presentation.displayable_assert_status(
+#             assertion_result.name,
+#             assertion_result.actual_original,
+#             "Success" if assertion_result.is_success else "Fail",
+#         )
+#     )
+#
+#     if assertion_result.is_success is False:
+#         print(Presentation.displayable_assert_message(assertion_result.message))
+#
+# return {}
+def present_assertion_result(ar: AssertResult) -> str:
+    status = "Success" if ar.is_success else "Fail"
+    resp = f"- Running `{ar.name}` on `{ar.actual_original}` [{status}]"
 
-    @staticmethod
-    def displayable_string(string: str) -> str:
-        if type(string) != str:
-            return str(string)
+    if ar.is_success is False:
+        resp += "\r\n---\r\n"
+        resp += ar.message
 
-        return string
+    return resp
 
-    @staticmethod
-    def displayable_assert_status(assert_name: str, actual: str, status: str) -> str:
-        return f"- Running `{assert_name}` on `{actual}` [{status}]"
 
-    @staticmethod
-    def displayable_assert_message(message: str) -> str:
-        return f"---\n{message}"
+def present_assertion_result_list(assertion_results: AssertResultList) -> str:
+    resp = ""
+
+    for item in assertion_results:
+        resp += present_assertion_result(item)
+        resp += "\r\n"
+
+    return resp
+
+
+def present_result(exposable: list) -> str:
+    printables: list[str] = []
+
+    for item in exposable:
+        if isinstance(item, AssertResult):
+            printables.append(
+                present_assertion_result(item)
+            )
+
+        elif isinstance(item, dict):
+            printables.append(present_dict(item))
+
+        elif isinstance(item, list):
+            printables.append(present_assertion_result_list(item))
+
+        else:
+            printables.append(str(item))
+
+    return "\r\n\r\n".join(printables)
