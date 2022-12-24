a8b5036c4f0f8d4e38759ed14baae39d5965f140
HSN <hasanlock@gmail.com>
2022-12-24 22:52:00 +0600

    feat: display exposable response with error response

--- chk/modules/testcase/main.py
+++ chk/modules/testcase/main.py
@@ -1,15 +1,23 @@
 """
 testcase module's driver
 """
+import sys
+from traceback import print_exc
+
+from chk.infrastructure.contexts import app
 from chk.infrastructure.file_loader import FileContext
-from chk.modules.testcase.entities import Testcase
 from chk.infrastructure.work import handle_worker
+
+from chk.modules.testcase.entities import Testcase
+from chk.modules.testcase.presentation import present_result
 
 
 def execute(file_ctx: FileContext) -> None:
     """Execute command functionality"""
+
     testcase = Testcase(file_ctx)
     try:
-        handle_worker(testcase)
-    except RuntimeError as ex:
-        raise SystemExit(ex) from ex
+        response = handle_worker(testcase)
+        app.print_fmt(response, present_result)
+    except RuntimeError:
+        print_exc(file=sys.stderr)
