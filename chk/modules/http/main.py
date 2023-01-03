1bb32d3409d8f139408e6bc5049b7a2aa7a7ab5d
HSN <hasanlock@gmail.com>
2023-01-03 20:42:30 +0600

    feat: execute with tests for http

--- chk/modules/http/main.py
+++ chk/modules/http/main.py
@@ -1,8 +1,7 @@
 """
 Main driver
 """
-import sys
-from traceback import print_exc
+from typing import Any
 
 from chk.infrastructure.contexts import app
 from chk.infrastructure.work import handle_worker
@@ -12,12 +11,15 @@ from chk.modules.http.entities import HttpSpec
 from chk.modules.http.presentation import present_result
 
 
-def execute(file_ctx: FileContext) -> None:
+def execute(file_ctx: FileContext, display: bool = True) -> Any:
     """Execute command functionality"""
 
     http_spec = HttpSpec(file_ctx)
     try:
         response = handle_worker(http_spec)
-        app.print_fmt(response, present_result)
-    except RuntimeError:
-        print_exc(file=sys.stderr)
+        if display:
+            app.print_fmt(response, present_result)
+
+        return response
+    except RuntimeError as err:
+        print("\r\n---\r\n", str(err)) if display else err
---
+++ tests/modules/http/http_main_test.py
@@ -0,0 +1,75 @@
+# type: ignore
+import sys
+from io import TextIOWrapper, BytesIO
+
+from chk.infrastructure.file_loader import FileContext
+from tests import RES_DIR
+from chk.modules.http.main import execute
+
+
+class TestExecute:
+    @staticmethod
+    def test_execute_pass_with_display():
+        file = RES_DIR + "pass_cases/GET-Plain.chk"
+        ctx = FileContext.from_file(file, {"result": False})
+
+        old_stdout = sys.stdout
+        sys.stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)
+
+        execute(ctx)
+
+        # get output
+        sys.stdout.seek(0)  # jump to the start
+        out = sys.stdout.read()  # read output
+
+        # restore stdout
+        sys.stdout.close()
+        sys.stdout = old_stdout
+
+        # assert
+        assert "Making request [Success]" in out
+        assert "Prepare exposable [Success]" in out
+
+    @staticmethod
+    def test_execute_pass_without_display():
+        file = RES_DIR + "pass_cases/GET-Plain.chk"
+        ctx = FileContext.from_file(file, {"result": True})
+
+        old_stdout = sys.stdout
+        sys.stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)
+
+        resp = execute(ctx, False)
+
+        # get output
+        sys.stdout.seek(0)  # jump to the start
+        out = sys.stdout.read()  # read output
+
+        # restore stdout
+        sys.stdout.close()
+        sys.stdout = old_stdout
+
+        # assert
+        assert len(out) == 0
+        assert isinstance(resp, list)
+
+    @staticmethod
+    def test_execute_fails_with_wrong_url():
+        file = RES_DIR + "fail_cases/GET-Plain-WrongURL.chk"
+        ctx = FileContext.from_file(file, {"result": False})
+
+        old_stdout = sys.stdout
+        sys.stdout = TextIOWrapper(BytesIO(), sys.stdout.encoding)
+
+        execute(ctx, True)
+
+        # get output
+        sys.stdout.seek(0)  # jump to the start
+        out = sys.stdout.read()  # read output
+
+        # restore stdout
+        sys.stdout.close()
+        sys.stdout = old_stdout
+
+        # assert
+        assert "Making request [Fail]" in out
+        assert "Connection error" in out
