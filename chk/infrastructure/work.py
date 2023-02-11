0fae85b92e4a1082a907381b5c0fb16293a90589
HSN <hasanlock@gmail.com>
2023-02-11 14:29:42 +0600

    refactor: remove unused interface RequestProcessorContract

--- chk/infrastructure/work.py
+++ chk/infrastructure/work.py
@@ -34,30 +34,3 @@ def handle_worker(worker: WorkerContract) -> object:
     """
     # try:
     return worker.__work__()
-
-
-class RequestProcessorContract(ABC):
-    """Contacts for work processing"""
-
-    def __initialize_process__(self):
-        self.request_args = {}
-
-    @abstractmethod
-    def __before_process__(self, args): pass
-
-    @abstractmethod
-    def __process__(self): pass
-
-
-def handle_request(processor: RequestProcessorContract, args: dict):
-    """
-    Run a RequestProcessorContract's contracts
-
-    :param processor: RequestProcessorContract
-    :param args: dict[str, object]
-    :return: dict
-    """
-
-    processor.__initialize_process__()
-    processor.__before_process__(args)
-    return processor.__process__()
