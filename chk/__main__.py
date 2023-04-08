754979eecbc08385a2f1556e942bfa753902e954
HSN <hasanlock@gmail.com>
2023-04-08 10:32:36 +0600

    feat: execute root command for new command

--- chk/__main__.py
+++ chk/__main__.py
@@ -1,6 +1,9 @@
 """
 chk cli
 """
-from chk.console.main import execute_root
+# from chk.console.main import execute_root
+from chk.console.chk import execute_root
 
-if __name__ == '__main__': execute_root()  # execute the root command
+if __name__ == "__main__":
+    # execute the root command
+    execute_root()
