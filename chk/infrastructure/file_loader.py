2a5dcba42122ff871014bcc4c60edd4e48c60532
HSN <hasanlock@gmail.com>
2023-04-20 12:47:32 +0600

    feat: add file loader utility

--- chk/infrastructure/file_loader.py
+++ chk/infrastructure/file_loader.py
@@ -2,11 +2,12 @@
 File loader utility
 """
 
-from pathlib import Path
 from types import MappingProxyType
 from typing import NamedTuple
+from pathlib import Path
 
-from yaml import safe_load  # type: ignore
+import json
+import yaml
 
 from chk.infrastructure import exception, mangle
 
@@ -19,7 +20,7 @@ class ChkFileLoader:
         """read yml data"""
         with open(file_name, "r", encoding="UTF-8") as yaml_file:
             try:
-                return safe_load(yaml_file)
+                return yaml.safe_load(yaml_file)
             except Exception as ex:
                 yaml_file.close()
                 raise SystemExit(
@@ -38,6 +39,68 @@ class ChkFileLoader:
     @staticmethod
     def get_mangled_name(file_name: str) -> tuple[str, str]:
         return mangle.filename(file_name), mangle.uniq_sha255(file_name)
+
+
+class FileLoader:
+    """File loader utility"""
+
+    @staticmethod
+    def is_file_ok(file_name: str, allowed_list: list | None = None) -> bool:
+        """Check if chk file exists, when extension is allowed
+        :param file_name: File name to check
+        :param allowed_list: List of allowed file extension, default is [".chk"]
+        :return: bool
+        :raises FileNotFoundError When file not found
+        :raises LookupError When file extension do not match
+        """
+        if not allowed_list:
+            allowed_list = [".chk"]
+
+        if not Path(file_name).is_file():
+            raise FileNotFoundError("File not found")
+
+        if Path(file_name).suffix not in allowed_list:
+            raise LookupError("File not allowed")
+
+        return True
+
+    @staticmethod
+    def load_yaml(file_name: str) -> dict:
+        """Try to load yaml file and return content as dictionary
+        :param file_name: str File name to be loaded
+        :return: File content as dictionary
+        """
+
+        with open(file_name, "r", encoding="UTF-8") as yaml_file:
+            try:
+                return yaml.safe_load(yaml_file)
+            except Exception as ex:
+                raise RuntimeError("YAML loading error.") from ex
+
+    @staticmethod
+    def load_json(file_name: str) -> dict:
+        """Try to load json file and return content as dictionary
+        :param file_name: str File name to be loaded
+        :return: File content as dictionary
+        """
+
+        with open(file_name, "r", encoding="UTF-8") as json_file_content:
+            try:
+                return json.loads(json_file_content.read())
+            except ValueError as ex:
+                raise RuntimeError("JSON loading error.") from ex
+
+    @staticmethod
+    def load_json_from_str(expected_json: str) -> dict:
+        """Try to load json file and return content as dictionary
+        :param expected_json: str JSON document to be loaded
+        :return: JSON document as dictionary
+        """
+
+        try:
+            return json.loads(expected_json)
+        except ValueError as ex:
+            raise RuntimeError("JSON loading error.") from ex
 
 
 class FileContext(NamedTuple):
--- tests/infrastructure/file_loader_test.py
+++ tests/infrastructure/file_loader_test.py
@@ -8,7 +8,12 @@ from pathlib import Path
 import pytest
 import tests
 
-from chk.infrastructure.file_loader import ChkFileLoader, FileContext, PathFrom
+from chk.infrastructure.file_loader import (
+    ChkFileLoader,
+    FileContext,
+    PathFrom,
+    FileLoader,
+)
 
 
 class TestChkFileLoader:
@@ -122,3 +127,83 @@ class TestPathFrom:
         assert path_3 in p.absolute("../some-folder/../bitcoin-usd-testcase-data.chk")
         assert path_4 in p.absolute("../some-folder/./bitcoin-usd-testcase-data.chk")
         assert path_2 in p.absolute("./some-folder////bitcoin-usd-testcase-data.chk")
+
+
+FILE_PATH = "tests/resources/storage/sample_config/"
+
+
+class TestFileLoaderIsFileOk:
+    """Create tests"""
+
+    def test_pass_with_default_allowed_list(self):
+        file_name = FILE_PATH + "bitcoin-usd.chk"
+        assert FileLoader.is_file_ok(file_name) is True
+
+    def test_fail_with_default_allowed_list_file_not_found(self):
+        file_name = FILE_PATH + "UserOk.yml"
+
+        with pytest.raises(FileNotFoundError):
+            FileLoader.is_file_ok(file_name)
+
+    def test_fail_with_default_allowed_list_not_allowed_file(self):
+        file_name = FILE_PATH + "UserOk.yaml"
+
+        with pytest.raises(LookupError):
+            FileLoader.is_file_ok(file_name)
+
+    def test_pass_with_given_allowed_list(self):
+        file_name = FILE_PATH + "UserOk.yaml"
+        assert FileLoader.is_file_ok(file_name, [".chk", ".yaml", ".yml"]) is True
+
+
+class TestFileLoaderLoadYaml:
+    """Create tests"""
+
+    def test_pass_valid_file(self):
+        file_name = FILE_PATH + "bitcoin-usd.chk"
+        loaded_content = FileLoader.load_yaml(file_name)
+
+        assert isinstance(loaded_content, dict)
+        assert isinstance(loaded_content.get("version"), str)
+
+    def test_fail_invalid_file(self):
+        file_name = FILE_PATH + "UserNotOk.chk"
+
+        with pytest.raises(RuntimeError):
+            FileLoader.load_yaml(file_name)
+
+
+class TestFileLoaderLoadJson:
+    """Create tests"""
+
+    def test_pass_valid_file(self):
+        file_name = FILE_PATH + "UserOk.json"
+        loaded_content = FileLoader.load_json(file_name)
+
+        assert isinstance(loaded_content, dict)
+        assert isinstance(loaded_content.get("version"), str)
+
+    def test_fail_invalid_file(self):
+        file_name = FILE_PATH + "UserNotOk.json"
+
+        with pytest.raises(RuntimeError):
+            FileLoader.load_json(file_name)
+
+
+class TestFileLoaderLoadJsonFromStr:
+    """Create tests"""
+
+    def test_pass_valid_file(self):
+        file_name = FILE_PATH + "UserOk.json"
+        with open(file_name, "r", encoding="UTF-8") as json_file_content:
+            loaded_content = FileLoader.load_json_from_str(json_file_content.read())
+
+            assert isinstance(loaded_content, dict)
+            assert isinstance(loaded_content.get("version"), str)
+
+    def test_fail_invalid_file(self):
+        file_name = FILE_PATH + "UserNotOk.json"
+
+        with open(file_name, "r", encoding="UTF-8") as json_file_content:
+            with pytest.raises(RuntimeError):
+                FileLoader.load_json_from_str(json_file_content.read())
