193e4d4a61674468f264dc715a7a44c039a3800e
HSN <hasanlock@gmail.com>
2023-04-26 11:16:21 +0600

    feat: json fetch request

---
+++ chk/plugins/json_fetcher.py
@@ -0,0 +1,38 @@
+"""
+JSON data fetcher
+"""
+import json
+from collections import UserDict
+
+from chk.plugins.fetcher import ApiResponse, fetch
+
+
+class JsonApiResponse(UserDict):
+    """JsonApiResponse"""
+
+    __slots__ = ("code", "info", "headers", "body")
+
+    code: int
+    info: str
+    headers: dict
+    body: str
+
+    @staticmethod
+    def from_api_response(resp: ApiResponse) -> "JsonApiResponse":
+        """Create JsonApiResponse from ApiResponse
+        Args:
+            resp (ApiResponse): ApiResponse object
+        """
+        try:
+            return JsonApiResponse(
+                code=resp["code"],
+                info=resp["info"],
+                headers=resp["headers"],
+                body=json.loads(resp["body"]),
+            )
+        except Exception as ex:
+            raise RuntimeError("response not json") from ex
+
+
+def fetch_json(params: dict) -> JsonApiResponse:
+    return JsonApiResponse.from_api_response(fetch(params))
