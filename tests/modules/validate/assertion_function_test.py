4c1bc745e44462a82a3d852f6b930d2a024196eb
HSN <hasanlock@gmail.com>
2023-06-30 08:28:57 +0600

    refactor: assert_equal impact on test

--- tests/modules/validate/assertion_function_test.py
+++ tests/modules/validate/assertion_function_test.py
@@ -6,25 +6,28 @@ Test module for assertion Functions mod
 import chk.modules.validate.assertion_function as asrt
 
 
-class TestAssertEquals:
+class TestAssertEqual:
     @staticmethod
-    def test_assert_equals_pass_with_scaler():
-        assert asrt.assert_equals(1, 1)[0]
-        assert asrt.assert_equals("abc", "abc")[0]
-        assert asrt.assert_equals(3.02, 3.02)[0]
-        assert asrt.assert_equals(True, True)[0]
-        assert asrt.assert_equals(False, False)[0]
+    def test_assert_equal_pass_with_scaler():
+        assert asrt.assert_equal(1, 1)[0]
+        assert asrt.assert_equal("abc", "abc")[0]
+        assert asrt.assert_equal(3.02, 3.02)[0]
+        assert asrt.assert_equal(True, True)[0]
+        assert asrt.assert_equal(False, False)[0]
 
     @staticmethod
-    def test_assert_equals_pass_with_collect():
-        assert asrt.assert_equals([1], [1])[0]
-        assert asrt.assert_equals(["abc"], ["abc"])[0]
-        assert asrt.assert_equals({"a": 3.02}, {"a": 3.02})[0]
-        assert asrt.assert_equals({"a": 3.02, "b": "c"}, {"a": 3.02, "b": "c"})[0]
+    def test_assert_equal_pass_with_collect():
+        assert asrt.assert_equal([1], [1])[0]
+        assert asrt.assert_equal(["abc"], ["abc"])[0]
+        assert asrt.assert_equal({"a": 3.02}, {"a": 3.02})[0]
+        assert asrt.assert_equal({"a": 3.02, "b": "c"}, {"a": 3.02, "b": "c"})[0]
 
     @staticmethod
-    def test_assert_equals_fails_with_non_equals():
-        is_success, response = asrt.assert_equals([1], [2])
+    def test_assert_equal_fails_with_non_equals():
+        is_success, response = asrt.assert_equal([1], [2])
+
+        assert not is_success
+        assert isinstance(response, AssertionError)
 
         assert not is_success
         assert isinstance(response, AssertionError)
