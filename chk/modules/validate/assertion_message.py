bffa12bf9a392f13486ded09597f194b9bcce0c0
HSN <hasanlock@gmail.com>
2023-08-19 16:42:45 +0600

    feat: assertion message for accepted, declined, empty, not_empty

--- chk/modules/validate/assertion_message.py
+++ chk/modules/validate/assertion_message.py
@@ -11,6 +11,22 @@ _AMessages = {
         "pass": "actual `{0}({1})` is equal to expected `{2}({3})`",
         "fail": "actual `{0}({1})` is not equal to expected `{2}({3})`",
     },
+    "accepted": {
+        "pass": "actual `{0}({1})` is an accepted value",
+        "fail": "actual `{0}({1})` is not an accepted value",
+    },
+    "declined": {
+        "pass": "actual `{0}({1})` is a declined value",
+        "fail": "actual `{0}({1})` is not a declined value",
+    },
+    "empty": {
+        "pass": "actual `{0}({1})` is an empty value",
+        "fail": "actual `{0}({1})` is not an empty value",
+    },
+    "not_empty": {
+        "pass": "actual `{0}({1})` is a non-empty value",
+        "fail": "actual `{0}({1})` is not a non-empty value",
+    },
 }
 
 
