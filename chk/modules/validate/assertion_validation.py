c093afacf3ee0fa3df674f37b00bce4e7839d83d
HSN <hasanlock@gmail.com>
2023-08-20 11:41:25 +0600

    feat: generic validation schema

--- chk/modules/validate/assertion_validation.py
+++ chk/modules/validate/assertion_validation.py
@@ -16,3 +16,41 @@ class AssertionEntityType(enum.StrEnum):
     BOOLEAN = "Boolean"
     INTEGER = "Integer"
 
+
+_generic_schema = {
+    "type": {
+        "required": True,
+        "empty": False,
+        "nullable": False,
+        "type": "string",
+        "allowed": [],
+    },
+    "actual": {
+        "required": True,
+        "empty": False,
+        "nullable": False,
+    },
+    "expected": {
+        "required": False,
+        "empty": False,
+        "nullable": True,
+    },
+    "cast_actual_to": {
+        "required": False,
+        "empty": False,
+        "nullable": False,
+        "type": "string",
+        "allowed": [
+            "int_or_flot",
+            "int",
+            "float",
+            "bool",
+            "none",
+            "dict",
+            "list",
+            "str",
+            "auto",
+        ],
+    },
+}
+
