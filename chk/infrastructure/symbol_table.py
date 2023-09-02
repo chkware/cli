50ff0fe0d537e29a5a6b39d0c011f17e24b920ee
HSN <hasanlock@gmail.com>
2023-09-02 11:41:52 +0600

    refactor: remove linear_replace with test in favour of StrTemplate

--- chk/infrastructure/symbol_table.py
+++ chk/infrastructure/symbol_table.py
@@ -61,35 +61,6 @@ class ExposableVariables(UserDict):
     """Holds data for a expose data"""
 
 
-def linear_replace(container: str, replace_with: dict) -> object:
-    """replace values found in string with typed return"""
-
-    if not isinstance(container, str):
-        return container
-
-    if len(replace_with) == 0:
-        return container
-
-    line_split = re.split(r"({{\s*[a-zA-Z0-9_.]+\s*}})", container)
-
-    if len(line_split) == 1 and container in line_split:
-        return container
-
-    line_strip = [
-        "".join(item.split() if "$" in item else item) for item in line_split if item
-    ]
-
-    for i, item in enumerate(line_strip):
-        if "{{" in item and "}}" in item:
-            value = data_get(replace_with, item.strip(" {}"), None)
-
-            line_strip[i] = value or item
-
-    return (
-        "".join([str(li) for li in line_strip])
-        if len(line_strip) > 1
-        else line_strip.pop()
-    )
 
 
 def replace_value_in_traversable(doc: dict | list, var_s: dict) -> dict | list:
