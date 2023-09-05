b68d296f82f03eb55298e80813e10f38dabdd3d3
HSN <hasanlock@gmail.com>
2023-09-05 11:04:59 +0600

    refactor: update fail message for count

--- chk/modules/validate/assertion_message.py
+++ chk/modules/validate/assertion_message.py
@@ -223,7 +223,7 @@ _AMessages = {
     "count": {
         "pass": "actual `{type_actual}({value_actual})` count is `{value_expected}`",
         "fail": "actual `{type_actual}({value_actual})` count is not `{value_expected}`",
-        "actual_no_len": "no count for actual `{type_actual}({value_actual})`",
+        "actual_no_len": "actual `{type_actual}({value_actual})` is not countable",
         "expected_not_int": "expected `{type_expected}({value_expected})` is not integer",
     },
 }
