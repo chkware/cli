b55a77b65401762f7245b62adfa94621e01f28bf
HSN <hasanlock@gmail.com>
2023-10-07 12:01:47 +0600

    feat: workflow driver with tests

--- chk/modules/workflow/__init__.py
+++ chk/modules/workflow/__init__.py
@@ -1,3 +1,36 @@
 """
 Workflow module
 """
+
+from collections import abc
+
+from icecream import ic
+
+from chk.infrastructure.file_loader import FileContext, ExecuteContext
+from chk.infrastructure.version import DocumentVersionMaker
+from chk.modules.workflow.domain import WorkflowDocument
+
+VERSION_SCOPE = ["workflow"]
+
+
+def execute(
+    ctx: FileContext, exec_ctx: ExecuteContext, cb: abc.Callable = lambda *args: ...
+) -> None:
+    """Run a workflow document
+
+    Args:
+        ctx: FileContext object to handle
+        exec_ctx: ExecuteContext
+        cb: Callable
+    """
+
+    wflow_doc = WorkflowDocument.from_file_context(ctx)
+    ic(wflow_doc)
+
+    # DocumentVersionMaker.verify_if_allowed(
+    #     DocumentVersionMaker.from_dict(wflow_doc.as_dict), VERSION_SCOPE
+    # )
+
+    # VersionedDocumentSupport.validate_with_schema(
+    #     HttpDocumentSupport.build_schema(), http_doc
+    # )
---
+++ tests/modules/workflow/wf_module_test.py
@@ -0,0 +1,19 @@
+# type: ignore
+"""
+Workflow module test
+"""
+
+from chk.infrastructure.file_loader import ExecuteContext
+from chk.modules.workflow import execute
+
+from tests import load_chk_file, load_file_ctx_for_file, get_exec_ctx, SPEC_DIR
+
+
+class TestWorkflowExecute:
+    @staticmethod
+    def test_execute(load_chk_file, load_file_ctx_for_file, get_exec_ctx):
+        filepath = f"{SPEC_DIR}workflow/simple-btc-wf.chk"
+        file_ctx = load_file_ctx_for_file(filepath)
+        execution_ctx = get_exec_ctx(False)
+
+        execute(file_ctx, execution_ctx)
