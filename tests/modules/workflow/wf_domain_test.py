082fa75d90c312b6facca21d7771d4c163988a61
HSN <hasanlock@gmail.com>
2023-10-07 12:01:24 +0600

    feat: workflow document with test

---
+++ tests/modules/workflow/wf_domain_test.py
@@ -0,0 +1,28 @@
+# type: ignore
+"""
+Domain test code
+"""
+
+from chk.modules.workflow import WorkflowDocument
+
+from tests import load_chk_file, load_file_ctx_for_file, SPEC_DIR
+
+
+class TestWorkflowDocument:
+    @staticmethod
+    def test_from_file_context_pass(load_chk_file, load_file_ctx_for_file):
+        filepath = f"{SPEC_DIR}workflow/simple-btc-wf.chk"
+        file_ctx = load_file_ctx_for_file(filepath)
+
+        wfdoc = WorkflowDocument.from_file_context(file_ctx)
+        wfdoc.id == "simpleBtcWf"
+
+    @staticmethod
+    def test_from_file_context_pass_noid(load_chk_file, load_file_ctx_for_file):
+        filepath = f"{SPEC_DIR}workflow/simple-btc-wf.chk"
+        file_ctx = load_file_ctx_for_file(filepath)
+
+        del file_ctx.document["id"] # remove document id
+
+        wfdoc = WorkflowDocument.from_file_context(file_ctx)
+        wfdoc.id == "simple-btc-wf" # check if document id defaults to file name
