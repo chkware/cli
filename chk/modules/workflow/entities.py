9857a437ebd536f7a65188cce2a04d81d3edcb25
HSN <hasanlock@gmail.com>
2023-10-08 13:00:15 +0600

    feat: entity for internal chkware validate task

--- chk/modules/workflow/entities.py
+++ chk/modules/workflow/entities.py
@@ -22,3 +22,23 @@ class ChkwareTask(BaseModel):
             raise AttributeError("ChkwareTask:from_dict Empty data given")
 
         return ChkwareTask(**data)
+
+
+class ChkwareValidateTask(ChkwareTask):
+    """Chkware validation task"""
+
+    class ChkwareTaskDataArgument(BaseModel):
+        """Chkware task data argument"""
+
+        data: dict
+
+    arguments: typing.Optional[ChkwareTaskDataArgument] = Field(default=None)
+
+    @staticmethod
+    def from_dict(data: dict) -> "ChkwareValidateTask":
+        """constractor"""
+
+        if not data:
+            raise AttributeError("ChkwareValidateTask:from_dict Empty data given")
+
+        return ChkwareValidateTask(**data)
--- tests/modules/workflow/wf_entities_test.py
+++ tests/modules/workflow/wf_entities_test.py
@@ -2,7 +2,7 @@
 """
 Testing Entities for workflow
 """
-from chk.modules.workflow.entities import ChkwareTask
+from chk.modules.workflow.entities import ChkwareTask, ChkwareValidateTask
 
 
 class TestChkwareTask:
@@ -12,3 +12,25 @@ class TestChkwareTask:
 
         assert task.uses == "fetch"
         assert task.file == "file.chk"
+
+
+class TestChkwareValidateTask:
+    @staticmethod
+    def test_from_dict_pass():
+        task = ChkwareValidateTask.from_dict(
+            {"uses": "validate", "file": "validate-file.chk"}
+        )
+
+        assert task.uses == "validate"
+        assert task.file == "validate-file.chk"
+        assert task.arguments is None
+
+        task = ChkwareValidateTask.from_dict(
+            {
+                "uses": "validate",
+                "file": "validate-file.chk",
+                "arguments": {"data": {"some": 12, "data": [1, "a"]}},
+            }
+        )
+
+        assert task.arguments and "data" in task.arguments.model_dump()
