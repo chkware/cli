360c82b96ebdcb5397688dd684256523edac7149
HSN <hasanlock@gmail.com>
2023-10-08 13:04:01 +0600

    feat: parsing tasks in workflow

--- chk/modules/workflow/domain.py
+++ chk/modules/workflow/domain.py
@@ -2,11 +2,13 @@
 Domain logics for workflow module
 """
 import pathlib
+import typing
 
 from pydantic import BaseModel, Field
 
 from chk.infrastructure.file_loader import FileContext
 from chk.infrastructure.helper import data_get
+from chk.modules.workflow.entities import ChkwareTask, ChkwareValidateTask
 
 VERSION_SCOPE = ["workflow"]
 
@@ -16,10 +18,21 @@ class VersionedDocument(BaseModel):
     version: str = Field(default_factory=str)
 
 
+class ParsedTask(BaseModel):
+    """Parsed tasks"""
+
+    name: str
+    uses: str
+    file: str
+    variables: dict = Field(default_factory=dict)
+    arguments: dict = Field(default=None)
+
+
 class WorkflowDocument(VersionedDocument):
     """WorkflowDocument"""
 
     id: str = Field(default_factory=str)
+    tasks: list[ChkwareTask | ChkwareValidateTask]
 
     @staticmethod
     def from_file_context(ctx: FileContext) -> "WorkflowDocument":
@@ -30,7 +43,31 @@ class WorkflowDocument(VersionedDocument):
 
         id_str = data_get(ctx.document, "id", pathlib.Path(ctx.filepath).stem)
 
-        if not (tasks_dct := data_get(ctx.document, "tasks")):
+        if not (tasks_lst := data_get(ctx.document, "tasks")):
             raise RuntimeError("`tasks:` not found.")
 
-        return WorkflowDocument(context=tuple(ctx), version=version_str, id=id_str)
+        if not isinstance(tasks_lst, list):
+            raise RuntimeError("`tasks:` is not list.")
+
+        tasks = []
+        for task in tasks_lst:
+            if not isinstance(task, dict):
+                raise RuntimeError("`tasks.*.item` should be map.")
+
+            parsed_task = ParsedTask(**task)
+
+            match parsed_task.uses:
+                case "fetch":
+                    tasks.append(ChkwareTask.from_dict(parsed_task.model_dump()))
+                case "validate":
+                    tasks.append(
+                        ChkwareValidateTask.from_dict(parsed_task.model_dump())
+                    )
+
+        return WorkflowDocument(
+            context=tuple(ctx), version=version_str, id=id_str, tasks=tasks
+        )
+
+
+class WorkflowDocumentSupport:
+    """Workflow document support"""
--- tests/modules/workflow/wf_domain_test.py
+++ tests/modules/workflow/wf_domain_test.py
@@ -2,8 +2,10 @@
 """
 Domain test code
 """
+from icecream import ic
 
 from chk.modules.workflow import WorkflowDocument
+from chk.modules.workflow.entities import ChkwareTask
 
 from tests import load_chk_file, load_file_ctx_for_file, SPEC_DIR
 
@@ -17,12 +19,18 @@ class TestWorkflowDocument:
         wfdoc = WorkflowDocument.from_file_context(file_ctx)
         assert wfdoc.id == "simpleBtcWf"
 
+        assert isinstance(wfdoc.tasks, list)
+        assert all(isinstance(i, ChkwareTask) for i in wfdoc.tasks)
+
     @staticmethod
     def test_from_file_context_pass_noid(load_chk_file, load_file_ctx_for_file):
         filepath = f"{SPEC_DIR}workflow/simple-btc-wf.chk"
         file_ctx = load_file_ctx_for_file(filepath)
 
-        del file_ctx.document["id"] # remove document id
+        del file_ctx.document["id"]  # remove document id
 
         wfdoc = WorkflowDocument.from_file_context(file_ctx)
-        assert wfdoc.id == "simple-btc-wf" # check if document id defaults to file name
+        assert wfdoc.id == "simple-btc-wf"  # check if document id defaults to file name
+
+        assert isinstance(wfdoc.tasks, list)
+        assert all(isinstance(i, ChkwareTask) for i in wfdoc.tasks)
