f0d030f5eed60581695d5ecf8a4c746ba7379fff
HSN <hasanlock@gmail.com>
2023-10-29 14:11:00 +0600

    feat: disallow extra data in input yaml

--- tests/modules/workflow/wf_entities_test.py
+++ tests/modules/workflow/wf_entities_test.py
@@ -2,6 +2,9 @@
 """
 Testing Entities for workflow
 """
+import pytest
+from pydantic import ValidationError
+
 from chk.modules.workflow.entities import ChkwareTask, ChkwareValidateTask
 
 
@@ -18,11 +21,11 @@ class TestChkwareValidateTask:
     @staticmethod
     def test_from_dict_pass():
         task = ChkwareValidateTask.from_dict(
-            {"uses": "validate", "file": "validate-file.chk"}
+            {"uses": "validate", "file": "validate-file-1.chk"}
         )
 
         assert task.uses == "validate"
-        assert task.file == "validate-file.chk"
+        assert task.file == "validate-file-1.chk"
         assert task.arguments is None
 
         task = ChkwareValidateTask.from_dict(
@@ -34,3 +37,27 @@ class TestChkwareValidateTask:
         )
 
         assert task.arguments and "data" in task.arguments.model_dump()
+
+    @staticmethod
+    def test_from_dict_fail():
+        with pytest.raises(ValidationError):
+            ChkwareValidateTask.from_dict(
+                {
+                    "uses": "validate",
+                    "file": "validate-file.chk",
+                    "arguments": {
+                        "data": {"some": 12, "data": [1, "a"]},
+                        "other_data": 1,
+                    },
+                }
+            )
+
+        with pytest.raises(ValidationError):
+            ChkwareValidateTask.from_dict(
+                {
+                    "uses": "validate",
+                    "file": "validate-file.chk",
+                    "other_data": 1,
+                    "arguments": {"data": {"some": 12, "data": [1, "a"]}},
+                }
+            )
