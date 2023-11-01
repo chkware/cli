3dc2aadeff321fc018a2902d02f920500293bfab
HSN <hasanlock@gmail.com>
2023-11-01 14:47:36 +0600

    refactor: move all domain code to main module

--- chk/modules/workflow/__init__.py
+++ chk/modules/workflow/__init__.py
@@ -2,16 +2,67 @@
 Workflow module
 """
 
+from __future__ import annotations
 from collections import abc
+import pathlib
 
+from pydantic import Field, ConfigDict
 from icecream import ic
 
+from chk.infrastructure.document import VersionedDocumentV2 as VersionedDocument
+from chk.modules.workflow.entities import (
+    ChkwareTask,
+    ChkwareValidateTask,
+    ParsedTask,
+    WorkflowUses,
+)
 from chk.infrastructure.file_loader import FileContext, ExecuteContext
+from chk.infrastructure.helper import data_get
 from chk.infrastructure.symbol_table import Variables, VariableTableManager
-from chk.modules.workflow.domain import WorkflowDocument, WorkflowDocumentSupport
+
 
 VERSION_SCOPE = ["workflow"]
 
+
+class WorkflowDocument(VersionedDocument):
+    """WorkflowDocument"""
+
+    model_config = ConfigDict(extra="forbid")
+
+    id: str = Field(default_factory=str)
+    tasks: list[ChkwareTask | ChkwareValidateTask]
+
+    @staticmethod
+    def from_file_context(ctx: FileContext) -> WorkflowDocument:
+        """Create a WorkflowDocument from FileContext"""
+
+        if not (version_str := data_get(ctx.document, "version")):
+            raise RuntimeError("`version:` not found.")
+
+        id_str = data_get(ctx.document, "id", pathlib.Path(ctx.filepath).stem)
+
+        if not (tasks_lst := data_get(ctx.document, "tasks")):
+            raise RuntimeError("`tasks:` not found.")
+
+        if not isinstance(tasks_lst, list):
+            raise RuntimeError("`tasks:` is not list.")
+
+        tasks = []
+        for task in tasks_lst:
+            if not isinstance(task, dict):
+                raise RuntimeError("`tasks.*.item` should be map.")
+
+            parsed_task = ParsedTask(**task)
+
+            match parsed_task.uses:
+                case "fetch":
+                    tasks.append(ChkwareTask.from_parsed_task(parsed_task))
+                case "validate":
+                    tasks.append(ChkwareValidateTask.from_parsed_task(parsed_task))
+
+        return WorkflowDocument(
+            context=tuple(ctx), version=version_str, id=id_str, tasks=tasks
+        )
 
 def execute(
     ctx: FileContext, exec_ctx: ExecuteContext, cb: abc.Callable = lambda *args: ...
