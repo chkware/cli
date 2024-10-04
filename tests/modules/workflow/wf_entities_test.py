452c674e0087d35d11af289272e6bf3ac002c30f
HSN <hasanlock@gmail.com>
2024-10-04 16:35:46 +0600

    fix: tests for wf tasks and services

--- tests/modules/workflow/wf_entities_test.py
+++ tests/modules/workflow/wf_entities_test.py
@@ -7,40 +7,50 @@ from pydantic import ValidationError
 
 from chk.modules.workflow.entities import ChkwareTask, ChkwareValidateTask
 
+fp = "/Users/mlbdmba21/Works/chkware/cli/tests/resources/storage/spec_docs/workflow_cases/simple"
+
 
 class TestChkwareTask:
     @staticmethod
     def test_from_dict_pass():
-        task = ChkwareTask.from_dict(
-            {"name": "Sample fetch", "uses": "fetch", "file": "file.chk"}
+
+        task = ChkwareTask(
+            fp,
+            **{
+                "name": "Sample fetch",
+                "uses": "fetch",
+                "file": "./coinstats-usd-validate.chk",
+            },
         )
 
         assert task.uses == "fetch"
-        assert task.file == "file.chk"
+        assert task.file == f"{fp}/coinstats-usd-validate.chk"
 
 
 class TestChkwareValidateTask:
     @staticmethod
     def test_from_dict_pass():
-        task = ChkwareValidateTask.from_dict(
-            {
+        task = ChkwareValidateTask(
+            fp,
+            **{
                 "name": "Sample validate",
                 "uses": "validate",
-                "file": "validate-file-1.chk",
-            }
+                "file": "./coinstats-usd-validate.chk",
+            },
         )
 
         assert task.uses == "validate"
-        assert task.file == "validate-file-1.chk"
-        assert task.arguments is None
+        assert task.file == f"{fp}/coinstats-usd-validate.chk"
+        assert isinstance(task.arguments, ChkwareValidateTask.ChkwareTaskDataArgument)
 
-        task = ChkwareValidateTask.from_dict(
-            {
+        task = ChkwareValidateTask(
+            fp,
+            **{
                 "name": "Sample validate - 2",
                 "uses": "validate",
-                "file": "validate-file.chk",
+                "file": "./coinstats-usd-validate.chk",
                 "arguments": {"data": {"some": 12, "data": [1, "a"]}},
-            }
+            },
         )
 
         assert task.arguments and "data" in task.arguments.model_dump()
@@ -48,25 +58,27 @@ class TestChkwareValidateTask:
     @staticmethod
     def test_from_dict_fail():
         with pytest.raises(ValidationError):
-            ChkwareValidateTask.from_dict(
-                {
+            ChkwareValidateTask(
+                fp,
+                **{
                     "name": "Sample validate - 3",
                     "uses": "validate",
-                    "file": "validate-file.chk",
+                    "file": "./coinstats-usd-validate.chk",
                     "arguments": {
                         "data": {"some": 12, "data": [1, "a"]},
                         "other_data": 1,
                     },
-                }
+                },
             )
 
         with pytest.raises(ValidationError):
-            ChkwareValidateTask.from_dict(
-                {
+            ChkwareValidateTask(
+                fp,
+                **{
                     "name": "Sample validate - 4",
                     "uses": "validate",
-                    "file": "validate-file.chk",
+                    "file": "./coinstats-usd-validate.chk",
                     "other_data": 1,
                     "arguments": {"data": {"some": 12, "data": [1, "a"]}},
-                }
+                },
             )
