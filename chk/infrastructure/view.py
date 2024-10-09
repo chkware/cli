c41bae750f91d2cb0f12ae7f13b3df0d72f94a48
HSN <hasanlock@gmail.com>
2024-10-09 10:43:18 +0600

    feat: new service for presentation
    - refactor: use the service in workflow

--- chk/infrastructure/view.py
+++ chk/infrastructure/view.py
@@ -5,6 +5,10 @@ import typing
 
 from pydantic import BaseModel
 
+from chk.infrastructure.file_loader import ExecuteContext
+from chk.infrastructure.helper import formatter
+from chk.infrastructure.symbol_table import ExecResponse
+
 
 class PresentationBuilder(BaseModel, abc.ABC):
     """Presentable signature"""
@@ -22,3 +26,20 @@ class PresentationBuilder(BaseModel, abc.ABC):
         """Signature to dump json"""
 
         raise NotImplementedError("Signature to dump json")
+
+
+class PresentationService:
+    """Comment service for presentation"""
+
+    @classmethod
+    def display(
+        cls,
+        ex_resp: ExecResponse,
+        exec_ctx: ExecuteContext,
+        presenter: type[PresentationBuilder],
+    ) -> None:
+        wfp = presenter(data=ex_resp)
+        if exec_ctx.options["format"]:
+            formatter(wfp.dump_fmt(), dump=exec_ctx.options["dump"])
+        else:
+            formatter(wfp.dump_json(), dump=exec_ctx.options["dump"])
