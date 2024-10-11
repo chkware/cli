6a1a99b9eb54da62e91fad828cd88b5d3155b2e9
HSN <hasanlock@gmail.com>
2024-10-11 23:01:30 +0600

    feat: use presentation service to dump

---
+++ chk/modules/validate/services.py
@@ -0,0 +1,34 @@
+"""
+General validation services
+"""
+
+from json import dumps
+
+from chk.infrastructure.view import PresentationBuilder
+from chk.modules.validate.entities import TestRunReport
+
+
+class ValidatePresenter(PresentationBuilder):
+
+    def dump_fmt(self) -> str:
+        """dump formatted string"""
+
+        display_items: list[str] = []
+
+        for key, item in self.data.exposed.items():
+            if key == "_asserts_response" and isinstance(item, TestRunReport):
+                display_items.append(item.as_fmt_str())
+            else:
+                display_items.append(dumps(item))
+
+        return "\n======\n".join(display_items)
+
+    def dump_json(self) -> str:
+        """dump json"""
+
+        display_items: list[dict] = []
+
+        for key, item in self.data.exposed.items():
+            display_items.append(dict(item))
+
+        return dumps(display_items)
