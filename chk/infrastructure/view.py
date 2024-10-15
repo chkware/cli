5b4822d1ed34c345bfea25601a41cae5bf0c06f9
HSN <hasanlock@gmail.com>
2024-10-15 15:00:23 +0600

    feat: add abstract signature and impl for error dump

--- chk/infrastructure/view.py
+++ chk/infrastructure/view.py
@@ -27,6 +27,18 @@ class PresentationBuilder(BaseModel, abc.ABC):
 
         raise NotImplementedError("Signature to dump json")
 
+    @abc.abstractmethod
+    def dump_error_fmt(self) -> str:
+        """Signature to dump formatted error string"""
+
+        raise NotImplementedError("Signature to dump formatted error string")
+
+    @abc.abstractmethod
+    def dump_error_json(self) -> str:
+        """Signature to dump error json"""
+
+        raise NotImplementedError("Signature to dump error json")
+
 
 class PresentationService:
     """Comment service for presentation"""
@@ -40,6 +52,12 @@ class PresentationService:
     ) -> None:
         wfp = presenter(data=ex_resp)
         if exec_ctx.options["format"]:
-            formatter(wfp.dump_fmt(), dump=exec_ctx.options["dump"])
+            if ex_resp.exception:
+                formatter(wfp.dump_error_fmt(), dump=exec_ctx.options["dump"])
+            else:
+                formatter(wfp.dump_fmt(), dump=exec_ctx.options["dump"])
         else:
-            formatter(wfp.dump_json(), dump=exec_ctx.options["dump"])
+            if ex_resp.exception:
+                formatter(wfp.dump_error_json(), dump=exec_ctx.options["dump"])
+            else:
+                formatter(wfp.dump_json(), dump=exec_ctx.options["dump"])
