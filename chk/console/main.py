1e09cea8f3da901120211d1b3433cb4fcc8996a4
HSN <hasanlock@gmail.com>
2024-10-20 15:25:42 +0600

    refactor: move services to other modules

--- chk/console/main.py
+++ chk/console/main.py
@@ -1,51 +1,17 @@
 """Commands"""
 
-import typing
-
 import click
 
 import chk.modules.fetch as fetch_executor
 import chk.modules.validate as validate_executor
 import chk.modules.workflow as workflow_executor
-from chk.infrastructure.file_loader import ExecuteContext, FileContext, FileLoader
-from chk.infrastructure.typing_extras import JsonDecodingError
+from chk.console.services import (
+    after_hook,
+    combine_initial_variables,
+    load_variables_as_dict,
+)
+from chk.infrastructure.file_loader import ExecuteContext, FileContext
 
-
-def load_variables_as_dict(json_str: str, **kwargs: typing.Any) -> dict:
-    """Reads a json string and converts  and returns the dict while doing validation"""
-
-    if json_str:
-        try:
-            return FileLoader.load_json_from_str(json_str)
-        except JsonDecodingError as err:
-            message = kwargs.get("except_msg") or "JSON loading error."
-            raise click.UsageError(str(message)) from err
-
-    return {}
-
-
-def combine_initial_variables(external_vars: str, **kwargs: typing.Any) -> dict:
-    """Reads a json string and converts to dict, and combines with env and dotenv
-    variables"""
-
-    return load_variables_as_dict(external_vars, **kwargs)
-
-
-def after_hook(resp: dict) -> None:
-    """Saves custom data from commands to global context bus
-
-    Args:
-        resp: object
-    """
-
-    if curr_ctx := click.get_current_context():
-        if curr_ctx.parent:
-            if not curr_ctx.parent.obj:
-                curr_ctx.parent.obj = {}
-
-            curr_ctx.parent.obj |= resp
-    else:
-        raise RuntimeError("Default context not found")
 VAR_ERROR_MSG = "-V, --variables accept values as JSON object"
 
 
