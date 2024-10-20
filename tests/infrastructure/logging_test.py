ae74f5bb906d9a71ed565f1d1503c5e68c52a172
HSN <hasanlock@gmail.com>
2024-10-20 13:45:23 +0600

    feat: generate session id

--- tests/infrastructure/logging_test.py
+++ tests/infrastructure/logging_test.py
@@ -2,14 +2,31 @@
 Logging Module Test
 """
 
-from chk.infrastructure.logging import LoggingManager
+from chk.infrastructure.logging import LoggingManager, create_session_id
 
 
 class TestGetLogFile:
     """TestGetLogFile"""
 
     @staticmethod
-    def test_get_log_file():
+    def create_cache_dir():
         """test_get_log_file"""
 
-        LoggingManager.get_log_file()
+        p = LoggingManager.create_cache_dir()
+
+        assert p.exists() and p.is_dir()
+
+    @staticmethod
+    def create_create_log_dir():
+        """test_get_log_file"""
+
+        p = LoggingManager.create_cache_dir()
+        q = LoggingManager.create_log_dir(p)
+
+        assert q.exists() and q.is_dir()
+
+
+def test_create_session_id():
+    new_session = create_session_id()
+
+    assert isinstance(new_session, str)
