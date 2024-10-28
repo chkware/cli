c768f73e9ffda3ef784063a42bbabf6219f870b9
HSN <hasanlock@gmail.com>
2024-10-28 12:18:51 +0600

    feat: implement die_with_error

--- chk/infrastructure/view.py
+++ chk/infrastructure/view.py
@@ -61,3 +61,16 @@ class PresentationService:
                 formatter(wfp.dump_error_json(), dump=exec_ctx.options["dump"])
             else:
                 formatter(wfp.dump_json(), dump=exec_ctx.options["dump"])
+
+def die_with_error(err: Exception, Presenter: type[PresentationBuilder], is_fmt: bool) -> None:
+    """die_with_error"""
+
+    prs = Presenter(data=None)
+
+    formatter(
+        prs.dump_error_fmt(err) if is_fmt else prs.dump_error_json(err),
+        dump=True,
+        is_err=True,
+    )
+
+    sys.exit(1)
