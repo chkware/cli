f0ebc474b9e2a4f322663897f944b81dd85f74f6
HSN <hasanlock@gmail.com>
2024-11-11 13:24:43 +0600

    refactor: move functionality to corresponding places

--- chk/modules/fetch/__init__.py
+++ chk/modules/fetch/__init__.py
@@ -1,4 +1,123 @@
 """
 Fetch module
 """
+import sys
+from collections import abc
 
+from chk.infrastructure.document import VersionedDocumentSupport
+from chk.infrastructure.file_loader import ExecuteContext, FileContext
+from chk.infrastructure.logging import debug, error_trace, with_catch_log
+from chk.infrastructure.symbol_table import ExecResponse, ExposeManager, \
+    VariableTableManager, \
+    Variables
+from chk.infrastructure.view import PresentationService, die_with_error
+from chk.modules.fetch.entities import FetchTask
+from chk.modules.fetch.services import FetchPresenter, HttpDocumentSupport
+
+
+@with_catch_log
+def call(file_ctx: FileContext, exec_ctx: ExecuteContext) -> ExecResponse:
+    """Call a http document"""
+
+    debug(file_ctx)
+    debug(exec_ctx)
+
+    r_exception: Exception | None = None
+    variable_doc = Variables()
+    output_data = Variables()
+    exposed_data = {}
+
+    try:
+        http_doc = HttpDocumentSupport.from_file_context(file_ctx)
+        debug(http_doc.model_dump_json())
+
+        VersionedDocumentSupport.validate_with_schema(
+            HttpDocumentSupport.build_schema(), http_doc
+        )
+
+        VariableTableManager.handle(variable_doc, http_doc, exec_ctx)
+        debug(variable_doc.data)
+
+        HttpDocumentSupport.process_request_template(http_doc, variable_doc)
+        debug(http_doc.model_dump_json())
+
+        # try:
+        response = HttpDocumentSupport.execute_request(http_doc)
+
+        output_data = Variables({"_response": response.model_dump()})
+        debug(output_data.data)
+
+        exposed_data = ExposeManager.get_exposed_replaced_data(
+            http_doc,
+            {**variable_doc.data, **output_data.data},
+        )
+        debug(exposed_data)
+
+    except Exception as ex:
+        r_exception = ex
+        error_trace(exception=sys.exc_info()).error(ex)
+
+    # TODO: instead if sending specific report items, and making presentable in other
+    #       module, we should prepare and long and short form of presentable that can be
+    #       loaded via other module
+
+    request_method, request_url = "", ""
+
+    if "request" in file_ctx.document:
+        if "method" in file_ctx.document["request"]:
+            request_method = file_ctx.document["request"]["method"]
+        if "url" in file_ctx.document["request"]:
+            request_url = file_ctx.document["request"]["url"]
+
+    return ExecResponse(
+        file_ctx=file_ctx,
+        exec_ctx=exec_ctx,
+        variables_exec=output_data,
+        variables=variable_doc,
+        exception=r_exception,
+        exposed=exposed_data,
+        report={
+            "is_success": r_exception is None,
+            "request_method": request_method,
+            "request_url": request_url,
+        },
+    )
+
+
+@with_catch_log
+def execute(
+    ctx: FileContext, exec_ctx: ExecuteContext, cb: abc.Callable = lambda *args: ...
+) -> None:
+    """Call with a http document
+
+    Args:
+        ctx: FileContext object to handle
+        exec_ctx: ExecuteContext
+        cb: Callable
+    """
+
+    try:
+        exr = call(file_ctx=ctx, exec_ctx=exec_ctx)
+        cb({ctx.filepath_hash: exr.variables_exec.data})
+        PresentationService.display(exr, exec_ctx, FetchPresenter)
+    except Exception as ex:
+        error_trace(exception=sys.exc_info()).error(ex)
+        die_with_error(ex, FetchPresenter, exec_ctx.options["format"])
+
+
+@with_catch_log
+def task_fetch(**kwargs: dict) -> ExecResponse:
+    """Task impl"""
+
+    if not (doc := kwargs.get("task", {})):
+        raise ValueError("Wrong task format given.")
+
+    _task = FetchTask(**doc)
+
+    return call(
+        FileContext.from_file(_task.file),
+        ExecuteContext(
+            options={"dump": True, "format": True},
+            arguments=_task.arguments | {"variables": _task.variables},
+        ),
+    )
---
+++ chk/modules/fetch/services.py
@@ -0,0 +1,296 @@
+"""
+Fetch module
+"""
+
+from __future__ import annotations
+
+import json
+import pathlib
+from urllib.parse import unquote, urlparse
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+from chk.infrastructure.file_loader import FileContext
+from chk.infrastructure.helper import data_get
+from chk.infrastructure.symbol_table import (
+    EXPOSE_SCHEMA as EXP_SCHEMA,
+    VARIABLE_SCHEMA as VAR_SCHEMA,
+    Variables,
+    replace_value,
+)
+from chk.infrastructure.version import DocumentVersionMaker, SCHEMA as VER_SCHEMA
+from chk.infrastructure.view import (
+    PresentationBuilder,
+)
+from chk.modules.fetch.entities import (
+    ApiResponseModel,
+    BearerAuthentication,
+    CTYPE_XML, HttpDocument,
+    HttpMethod,
+    RequestConfigNode, SCHEMA, VERSION_SCOPE,
+)
+
+
+def allowed_method(value: str) -> bool:
+    """Validate if given method is allowed
+
+    Raises:
+        ValueError: When unsupported method found
+    """
+
+    if value not in set(method for method in HttpMethod):
+        raise ValueError("Unsupported method")
+
+    return True
+
+
+def allowed_url(value: str) -> bool:
+    """Validate if given URL is allowed"""
+
+    parsed_url = urlparse(value)
+
+    if all([parsed_url.scheme, parsed_url.netloc]) is False:
+        raise ValueError("Invalid `url`")
+
+    if parsed_url.scheme not in ["http", "https"]:
+        raise ValueError("Invalid `url` scheme. http and https allowed")
+
+    return True
+
+
+class HttpRequestArgCompiler:
+    """HttpRequestArgCompiler"""
+
+    @staticmethod
+    def add_url_and_method(request_data: dict, request_arg: dict) -> None:
+        """add default request url and request method"""
+        if RequestConfigNode.METHOD not in request_data:
+            raise KeyError("required key `method:` not found")
+
+        if allowed_method(request_data[RequestConfigNode.METHOD]):
+            request_arg["method"] = request_data.get(RequestConfigNode.METHOD)
+
+        if RequestConfigNode.URL not in request_data:
+            raise KeyError("required key `url:` not found")
+
+        if allowed_url(request_data[RequestConfigNode.URL]):
+            request_arg["url"] = request_data.get(RequestConfigNode.URL)
+
+    @staticmethod
+    def add_query_string(request_data: dict, request_arg: dict) -> None:
+        """add query string"""
+        if (params := request_data.get(RequestConfigNode.PARAMS)) is not None:
+            request_arg["params"] = params
+
+    @staticmethod
+    def add_headers(request_data: dict, request_arg: dict) -> None:
+        """add custom header"""
+        if (headers := request_data.get(RequestConfigNode.HEADERS)) is not None:
+            request_arg["headers"] = headers
+
+    @staticmethod
+    def add_authorization(request_data: dict, request_arg: dict) -> None:
+        """handle authorization header"""
+        # handle basic auth
+        if (tag_ba := request_data.get(RequestConfigNode.AUTH_BA)) is not None:
+            request_arg["auth"] = HTTPBasicAuth(
+                tag_ba.get(RequestConfigNode.AUTH_BA_USR),
+                tag_ba.get(RequestConfigNode.AUTH_BA_PAS),
+            )
+
+        # handle bearer auth
+        if (tag_be := request_data.get(RequestConfigNode.AUTH_BE)) is not None:
+            request_arg["auth"] = BearerAuthentication(
+                tag_be.get(RequestConfigNode.AUTH_BE_TOK)
+            )
+
+    @staticmethod
+    def add_body(request_data: dict, request_arg: dict) -> None:
+        """add body"""
+        if (body := request_data.get(RequestConfigNode.BODY_FRM)) is not None:
+            request_arg["data"] = dict(body)
+        elif (body := request_data.get(RequestConfigNode.BODY_FRM_DAT)) is not None:
+            data = {}
+            files = {}
+
+            for key, val in dict(body).items():
+                if isinstance(val, str) and val.startswith("file://"):
+                    path_parsed = urlparse(val)
+                    path = unquote(path_parsed.path)
+                    netloc = unquote(path_parsed.netloc)
+
+                    filepath = pathlib.Path(f"{netloc}{path}")
+                    if not filepath.expanduser().exists():
+                        raise FileNotFoundError(f"path `{val}` do not exists")
+
+                    files[key] = str(filepath.expanduser().resolve())
+                else:
+                    data[key] = val
+
+            request_arg["data"] = data
+            request_arg["files"] = files
+
+        elif (body := request_data.get(RequestConfigNode.BODY_JSN)) is not None:
+            request_arg["json"] = dict(body)
+        elif (body := request_data.get(RequestConfigNode.BODY_XML)) is not None:
+            if "headers" not in request_arg:
+                request_arg["headers"] = {}
+
+            if (
+                "content-type" not in request_arg["headers"]
+                and "Content-Type" not in request_arg["headers"]
+            ):
+                request_arg["headers"]["content-type"] = CTYPE_XML
+
+            request_arg["data"] = body
+        elif (body := request_data.get(RequestConfigNode.BODY_TXT)) is not None:
+            if "headers" not in request_arg:
+                request_arg["headers"] = {}
+
+            if (
+                "content-type" not in request_arg["headers"]
+                and "Content-Type" not in request_arg["headers"]
+            ):
+                request_arg["headers"]["content-type"] = "text/plain"
+
+            request_arg["data"] = str(body)
+
+    @staticmethod
+    def add_generic_args(request_data: dict, request_arg: dict) -> None:
+        """add default request parameters regardless of method"""
+        HttpRequestArgCompiler.add_url_and_method(request_data, request_arg)
+        HttpRequestArgCompiler.add_query_string(request_data, request_arg)
+        HttpRequestArgCompiler.add_headers(request_data, request_arg)
+        HttpRequestArgCompiler.add_authorization(request_data, request_arg)
+        HttpRequestArgCompiler.add_body(request_data, request_arg)
+
+
+class HttpDocumentSupport:
+    """Service class for HttpDocument"""
+
+    @staticmethod
+    def from_file_context(ctx: FileContext) -> HttpDocument:
+        """Create a HttpDocument from FileContext
+        :param ctx: FileContext to create the HttpDocument from
+        """
+
+        doc_ver = DocumentVersionMaker.from_dict(ctx.document)
+        DocumentVersionMaker.verify_if_allowed(doc_ver, VERSION_SCOPE)
+
+        if not (version_str := data_get(ctx.document, "version")):
+            raise RuntimeError("`version:` not found.")
+
+        if not (request_dct := data_get(ctx.document, "request")):
+            raise RuntimeError("`request:` not found.")
+
+        # @TODO keep `context`, `version` as object
+        # @TODO implement __repr__ for WorkflowDocument
+        return HttpDocument(
+            context=tuple(ctx),
+            version=version_str,
+            request=request_dct,
+        )
+
+    @staticmethod
+    def execute_request(http_doc: HttpDocument) -> ApiResponseModel:
+        """Execute http request from given HttpDocument
+
+        Args:
+            http_doc (HttpDocument): Http request document object
+
+        Returns:
+            dict: Returns response for http request
+        """
+
+        if not http_doc:
+            raise ValueError("Empty document found.")
+
+        request_args: dict = {}
+
+        HttpRequestArgCompiler.add_generic_args(http_doc.request, request_args)
+
+        return ApiResponseModel.from_response(requests.request(**request_args))
+
+    @staticmethod
+    def process_request_template(http_doc: HttpDocument, variables: Variables) -> None:
+        """Replace variables in request body
+
+        Args:
+            http_doc: HttpDocument, for the request body
+            variables: Variables, variable base
+        """
+
+        http_doc.request = replace_value(http_doc.request, variables.data)
+
+    @staticmethod
+    def build_schema() -> dict:
+        """Validate a http document with given json-schema
+
+        Returns:
+            dict: Containing http document schema
+        """
+
+        return {**VER_SCHEMA, **SCHEMA, **VAR_SCHEMA, **EXP_SCHEMA}
+
+
+class FetchPresenter(PresentationBuilder):
+    """FetchPresenter"""
+
+    def dump_error_json(self, err: object = None) -> str:
+        """dump_error_json"""
+
+        if not err:
+            err = self.data.exception
+
+        return json.dumps({"error": (repr(err) if err else "Unspecified error")})
+
+    def dump_error_fmt(self, err: object = None) -> str:
+        """dump fmt error str"""
+
+        if not err:
+            err = self.data.exception
+
+        return (
+            f"Fetch error\n------\n{repr(err)}"
+            if err
+            else "Fetch error\n------\nUnspecified error"
+        )
+
+    def dump_json(self) -> str:
+        """dump json"""
+
+        displayables: list[object] = []
+
+        for key, expose_item in self.data.exposed.items():
+            if key == RequestConfigNode.LOCAL and all(
+                [
+                    item in expose_item.keys()
+                    for item in ["code", "info", "headers", "body"]
+                ]
+            ):
+                resp = ApiResponseModel.from_dict(**expose_item)
+                displayables.append(resp.model_dump())
+            else:
+                displayables.append(expose_item)
+
+        return json.dumps(displayables)
+
+    def dump_fmt(self) -> str:
+        """dump fmt string"""
+
+        displayables: list[str] = []
+
+        for key, expose_item in self.data.exposed.items():
+            if key == RequestConfigNode.LOCAL and all(
+                [
+                    item in expose_item.keys()
+                    for item in ["code", "info", "headers", "body"]
+                ]
+            ):
+                resp = ApiResponseModel.from_dict(**expose_item)
+                displayables.append(resp.as_fmt_str())
+            else:
+                displayables.append(json.dumps(expose_item))
+
+        return "\n======\n".join(displayables)
