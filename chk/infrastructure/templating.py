71f0b356442580f382cf395e3ea515398895da94
HSN <hasanlock@gmail.com>
2025-02-25 09:56:25 +0600

    feat: new jinja fromjson filter added

--- chk/infrastructure/templating.py
+++ chk/infrastructure/templating.py
@@ -2,6 +2,10 @@
 Templating module
 """
 
+import json
+import typing
+
+from jinja2 import TemplateError
 from jinja2.environment import Template
 from jinja2.nativetypes import NativeEnvironment
 
@@ -29,6 +33,8 @@ class JinjaTemplate:
             comment_end_string="#>",
         )
 
+        env.filters["fromjson"] = filter_fromjson
+
         return env.from_string(template)
 
 
@@ -37,3 +43,15 @@ def is_template_str(tpl: str) -> bool:
 
     _dm_sets = [("<%", "%>"), ("<@", "@>"), ("<#", "#>")]
     return any([_dm_set[0] in tpl and _dm_set[1] in tpl for _dm_set in _dm_sets])
+
+######################################
+# Jinja Filters
+######################################
+
+def filter_fromjson(value: typing.Any) -> str:
+    """Convert a JSON string to a Python dictionary."""
+
+    try:
+        return json.loads(value)
+    except json.JSONDecodeError as e:
+        raise TemplateError(f"Invalid JSON string: {e}") from e
